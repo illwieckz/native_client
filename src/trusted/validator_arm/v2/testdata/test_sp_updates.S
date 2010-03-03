illwieckz/native_client

commit 52a7bc502ce20e4186909a5875d724f583cd1cb7

This addresses http://code.google.com/p/nativeclient/issues/detail?id=312 .
Added a test to demonstrate the validator hole discussed in that bug (test_sp_updates.S, test_sp_updates.err).  The test failed, confirming the report.

Turns out I had artificially combined several classes of loads into a single ClassDecoder, and we were losing information in the process.  I split the instructions into five new classes; thankfully some OO kept the LOC in check (armv7.table, armv7-opt.table, inst_classes.h, inst_classes.cc).  The new decoder classes distinguish between register- and immediate-offset address generation, and thus distinguish the two types of writeback that can result.

With this change the new test passes.  I'm still squinting at the edges of this bug -- I suspect we might have other beasties nearby.

Review URL: http://codereview.chromium.org/653001

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1562 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/validator_arm/v2/inst_classes.cc
@@ -15,12 +15,20 @@
 namespace nacl_arm_dec {
 
 
+/*
+ * Breakpoint
+ */
+
 bool Breakpoint::is_literal_pool_head(const Instruction i) const {
   return i.condition() == Instruction::AL
       && i.bits(19, 8) == 0x777
       && i.bits(3, 0) == 0x7;
 }
 
+
+/*
+ * Data processing and arithmetic
+ */
 
 SafetyLevel DataProc::safety(const Instruction i) const {
   if (defs(i)[kRegisterPc]) {
@@ -90,11 +98,19 @@ RegisterList SatAddSub::defs(const Instruction i) const {
 }
 
 
+/*
+ * MSR
+ */
+
 RegisterList MoveToStatusRegister::defs(const Instruction i) const {
   UNREFERENCED_PARAMETER(i);
   return kRegisterFlags;
 }
 
+
+/*
+ * Stores
+ */
 
 SafetyLevel StoreImmediate::safety(const Instruction i) const {
   // Don't let addressing writeback alter PC.
@@ -137,29 +153,62 @@ Register StoreExclusive::base_address_register(const Instruction i) const {
 }
 
 
-SafetyLevel Load::safety(const Instruction i) const {
+/*
+ * Loads
+ */
+
+bool AbstractLoad::writeback(const Instruction i) const {
+  // Algorithm defined in pseudocode in ARM instruction set spec
+  // Valid for LDR, LDR[BH], LDRD
+  // Not valid for LDREX and kin
+  bool p = i.bit(24);
+  bool w = i.bit(21);
+  return !p | w;
+}
+
+SafetyLevel AbstractLoad::safety(const Instruction i) const {
   if (defs(i)[kRegisterPc]) return FORBIDDEN_OPERANDS;
 
   return MAY_BE_SAFE;
 }
 
-RegisterList Load::defs(const Instruction i) const {
+RegisterList AbstractLoad::defs(const Instruction i) const {
   return i.reg(15, 12) + immediate_addressing_defs(i);
 }
 
-RegisterList Load::immediate_addressing_defs(const Instruction i) const {
-  bool p = i.bit(24);
-  bool w = i.bit(21);
-  if (!p) return i.reg(19, 16);
-  if (w) return i.reg(19, 16);
-  return kRegisterNone;
+
+RegisterList LoadRegister::defs(const Instruction i) const {
+  if (writeback(i)) {
+    Register rn(i.bits(19, 16));
+    return AbstractLoad::defs(i) + rn;
+  } else {
+    return AbstractLoad::defs(i);
+  }
 }
 
 
-RegisterList LoadDouble::defs(const Instruction i) const {
-  // For rN in bits 15:12, this writes both rN and rN + 1
-  return i.reg(15, 12) + Register(i.bits(15, 12) + 1)
-      + immediate_addressing_defs(i);
+RegisterList LoadImmediate::immediate_addressing_defs(const Instruction i)
+    const {
+  if (writeback(i)) {
+    Register rn(i.bits(19, 16));
+    return rn;
+  } else {
+    return kRegisterNone;
+  }
+}
+
+
+RegisterList LoadDoubleI::defs(const Instruction i) const {
+  return AbstractLoad::defs(i) + Register(i.bits(15, 12) + 1);
+}
+
+
+RegisterList LoadDoubleR::defs(const Instruction i) const {
+  return AbstractLoad::defs(i) + Register(i.bits(15, 12) + 1);
+}
+
+RegisterList LoadDoubleExclusive::defs(const Instruction i) const {
+  return LoadExclusive::defs(i) + Register(i.bits(15, 12) + 1);
 }
 
 
@@ -186,6 +235,10 @@ RegisterList LoadMultiple::immediate_addressing_defs(
   return i.bit(21)? i.reg(19, 16) : kRegisterNone;
 }
 
+
+/*
+ * Vector load/stores
+ */
 
 SafetyLevel VectorLoad::safety(Instruction i) const {
   if (defs(i)[kRegisterPc]) return FORBIDDEN_OPERANDS;
@@ -228,6 +281,10 @@ Register VectorStore::base_address_register(Instruction i) const {
 }
 
 
+/*
+ * Coprocessors
+ */
+
 SafetyLevel CoprocessorOp::safety(Instruction i) const {
   if (defs(i)[kRegisterPc]) return FORBIDDEN_OPERANDS;
   uint32_t cpid = i.bits(11, 8);
@@ -264,15 +321,6 @@ Register StoreCoprocessor::base_address_register(Instruction i) const {
 }
 
 
-RegisterList BxBlx::defs(const Instruction i) const {
-  return kRegisterPc + (i.bit(5)? kRegisterLink : kRegisterNone);
-}
-
-Register BxBlx::branch_target_register(const Instruction i) const {
-  return i.reg(3, 0);
-}
-
-
 RegisterList MoveFromCoprocessor::defs(Instruction i) const {
   Register rd = i.reg(15, 12);
   if (rd == kRegisterPc) {
@@ -285,6 +333,19 @@ RegisterList MoveFromCoprocessor::defs(Instruction i) const {
 
 RegisterList MoveDoubleFromCoprocessor::defs(Instruction i) const {
   return i.reg(15, 12) + i.reg(19, 16);
+}
+
+
+/*
+ * Control flow
+ */
+
+RegisterList BxBlx::defs(const Instruction i) const {
+  return kRegisterPc + (i.bit(5)? kRegisterLink : kRegisterNone);
+}
+
+Register BxBlx::branch_target_register(const Instruction i) const {
+  return i.reg(3, 0);
 }
 
 

src/trusted/validator_arm/v2/testdata/test_sp_updates.S
@@ -43,3 +43,10 @@ bundle3:
 
 bundle4:
   bic sp, sp, #MASK         @ ...broken across bundle (ERROR).
+
+  .align 4
+bundle5:
+  ldr r0, [sp], r1        @ Attempt to update SP using register displacement,
+  nop                     @ without immediately masking afterwards: ERROR.
+  ldr r0, [sp], r1        @ Same SP update,
+  bic sp, sp, #0xC0000000 @ with mask, should pass.

src/trusted/validator_arm/v2/testdata/test_sp_updates.err
@@ -2,3 +2,4 @@ ncval: 70000078 6 kProblemUnsafeDataWrite 00000000
 ncval: 70000088 6 kProblemUnsafeDataWrite 00000000
 ncval: 7000008C 6 kProblemPatternCrossesBundle 00000000
 ncval: 7000009C 6 kProblemPatternCrossesBundle 00000000
+ncval: 700000B0 6 kProblemUnsafeDataWrite 00000000

src/third_party/arm-isa/armv7.table
@@ -130,38 +130,38 @@ op1(22:21) op(5)
 
 -- extra_load_store (See Section A5.2.8)
 op2(6:5) op1(24:20) Rn(19:16)
-01       xx0x0      -         =Forbidden  # STRH(register)
-"        xx0x1      -         =Load  # LDRH(register)
-"        xx1x0      -         =StoreImmediate  # STRH(immediate)
-"        xx1x1      ~1111     =Load  # LDRH(immediate)
-"        "          1111      =Load  # LDRH(literal)
-10       xx0x0      -         =LoadDouble(v5TE)  # LDRD(register)
-"        xx0x1      -         =Load  # LDRSB(register)
-"        xx1x0      ~1111     =LoadDouble(v5TE)  # LDRD(immediate)
-"        "          1111      =LoadDouble(v5TE)  # LDRD(literal)
-"        xx1x1      ~1111     =Load  # LDRSB(immediate)
-"        "          1111      =Load  # LDRSB(literal)
-11       xx0x0      -         =Forbidden  # STRD(register)
-"        xx0x1      -         =Load  # LDRSH(register)
-"        xx1x0      -         =StoreImmediate  # STRD(immediate)
-"        xx1x1      ~1111     =Load  # LDRSH(immediate)
-"        "          1111      =Load  # LDRSH(literal)
+01       xx0x0      -         =Forbidden          # STRH(register)
+"        xx0x1      -         =LoadRegister       # LDRH(register)
+"        xx1x0      -         =StoreImmediate     # STRH(immediate)
+"        xx1x1      ~1111     =LoadImmediate      # LDRH(immediate)
+"        "          1111      =LoadImmediate      # LDRH(literal)
+10       xx0x0      -         =LoadDoubleR(v5TE)  # LDRD(register)
+"        xx0x1      -         =LoadRegister       # LDRSB(register)
+"        xx1x0      ~1111     =LoadDoubleI(v5TE)  # LDRD(immediate)
+"        "          1111      =LoadDoubleI(v5TE)  # LDRD(literal)
+"        xx1x1      ~1111     =LoadImmediate      # LDRSB(immediate)
+"        "          1111      =LoadImmediate      # LDRSB(literal)
+11       xx0x0      -         =Forbidden          # STRD(register)
+"        xx0x1      -         =LoadRegister       # LDRSH(register)
+"        xx1x0      -         =StoreImmediate     # STRD(immediate)
+"        xx1x1      ~1111     =LoadImmediate      # LDRSH(immediate)
+"        "          1111      =LoadImmediate      # LDRSH(literal)
 
 # Unprivileged load-store table omitted: modeled as Forbidden.
 # They are not expected in user code.
 
 -- sync (See Section A5.2.10)
 op(23:20)
-0x00      =Deprecated      # SWP, SWPB  TODO(cbiffle): model these
-1000      =StoreExclusive(v6)  # STREX
-1001      =Load(v6)            # LDREX
-1010      =StoreExclusive(v6K)  # STREXD
-1011      =LoadDouble(v6K)      # LDREXD
-1100      =StoreExclusive(v6K)  # STREXB
-1101      =Load(v6K)            # LDREXB
-1110      =StoreExclusive(v6K)  # STREXH
-1111      =Load(v6K)            # LDREXH
--         =Undefined(v6K)       # Note on page A5-16
+0x00      =Deprecated      # SWP, SWPB  TODO(cbiffle): model these?
+1000      =StoreExclusive(v6)        # STREX
+1001      =LoadExclusive(v6)         # LDREX
+1010      =StoreExclusive(v6K)       # STREXD
+1011      =LoadDoubleExclusive(v6K)  # LDREXD
+1100      =StoreExclusive(v6K)       # STREXB
+1101      =LoadExclusive(v6K)        # LDREXB
+1110      =StoreExclusive(v6K)       # STREXH
+1111      =LoadExclusive(v6K)        # LDREXH
+-         =Undefined(v6K)            # Note on page A5-16
 
 -- msr_and_hints (See Section A5.2.11)
 op(22) op1(19:16) op2(7:0)
@@ -202,18 +202,18 @@ A(25) op1(24:20) B(4) Rn(19:16)
 1     xx0x0      0    -        =Forbidden       # STR(register)
 0     0x010      -    -        =Forbidden       # STRT
 1     0x010      0    -        "
-0     xx0x1      -    ~1111    =Load            # LDR(immediate)
-"     "          "    1111     =Load            # LDR(literal)
-1     xx0x1      0    -        =Load            # LDR(register)
+0     xx0x1      -    ~1111    =LoadImmediate   # LDR(immediate)
+"     "          "    1111     =LoadImmediate   # LDR(literal)
+1     xx0x1      0    -        =LoadRegister    # LDR(register)
 0     0x011      -    -        =Forbidden       # LDRT
 1     0x011      0    -        "
 0     xx1x0      -    -        =StoreImmediate  # STRB(immediate)
 1     xx1x0      0    -        =Forbidden       # STRB(register)
 0     0x110      -    -        =Forbidden       # STRBT
 1     0x110      0    -        "
-0     xx1x1      -    ~1111    =Load            # LDRB(immediate)
-"     "          "    1111     =Load            # LDRB(literal)
-1     xx1x1      0    -        =Load            # LDRB(register)
+0     xx1x1      -    ~1111    =LoadImmediate   # LDRB(immediate)
+"     "          "    1111     =LoadImmediate   # LDRB(literal)
+1     xx1x1      0    -        =LoadRegister    # LDRB(register)
 0     0x111      -    -        =Forbidden       # LDRBT
 1     0x111      0    -        "
 

src/trusted/validator_arm/v2/armv7-opt.table
@@ -61,21 +61,24 @@ xxxxxxxxx10xxxxxxxxxxxxxxxxxxxxx  =LongMultiply(v5TE)
 
 -- extra_load_store (See Section A5.2.8)
 pat0(31:0)                        pat1(31:0)                        
-xxxxxxxxxxxxxxxxxxxxxxxxxx1xxxxx  xxxxxxxxx1x0xxxxxxxxxxxxxxxxxxxx  =StoreImmediate
-xxxxxxxxxxxxxxxxxxxxxxxxx10xxxxx  xxxxxxxxxxx1xxxxxxxxxxxxxxxxxxxx  =Load
-xxxxxxxxxxxxxxxxxxxxxxxxxx1xxxxx  xxxxxxxxxxx1xxxxxxxxxxxxxxxxxxxx  =Load
-xxxxxxxxxxxxxxxxxxxxxxxxx10xxxxx  xxxxxxxxxxx0xxxxxxxxxxxxxxxxxxxx  =LoadDouble(v5TE)
 xxxxxxxxxxxxxxxxxxxxxxxxxx1xxxxx  xxxxxxxxx0x0xxxxxxxxxxxxxxxxxxxx  =Forbidden
+xxxxxxxxxxxxxxxxxxxxxxxxx10xxxxx  xxxxxxxxx0x1xxxxxxxxxxxxxxxxxxxx  =LoadRegister
+xxxxxxxxxxxxxxxxxxxxxxxxxx1xxxxx  xxxxxxxxx0x1xxxxxxxxxxxxxxxxxxxx  =LoadRegister
+xxxxxxxxxxxxxxxxxxxxxxxxx10xxxxx  xxxxxxxxx0x0xxxxxxxxxxxxxxxxxxxx  =LoadDoubleR(v5TE)
+xxxxxxxxxxxxxxxxxxxxxxxxxx1xxxxx  xxxxxxxxx1x0xxxxxxxxxxxxxxxxxxxx  =StoreImmediate
+xxxxxxxxxxxxxxxxxxxxxxxxx10xxxxx  xxxxxxxxx1x1xxxxxxxxxxxxxxxxxxxx  =LoadImmediate
+xxxxxxxxxxxxxxxxxxxxxxxxxx1xxxxx  xxxxxxxxx1x1xxxxxxxxxxxxxxxxxxxx  =LoadImmediate
+xxxxxxxxxxxxxxxxxxxxxxxxx10xxxxx  xxxxxxxxx1x0xxxxxxxxxxxxxxxxxxxx  =LoadDoubleI(v5TE)
 
 -- sync (See Section A5.2.10)
 pat0(31:0)                        
 xxxxxxxx0x00xxxxxxxxxxxxxxxxxxxx  =Deprecated
+xxxxxxxx11x1xxxxxxxxxxxxxxxxxxxx  =LoadExclusive(v6K)
+xxxxxxxx1011xxxxxxxxxxxxxxxxxxxx  =LoadDoubleExclusive(v6K)
 xxxxxxxx1100xxxxxxxxxxxxxxxxxxxx  =StoreExclusive(v6K)
 xxxxxxxx1x10xxxxxxxxxxxxxxxxxxxx  =StoreExclusive(v6K)
-xxxxxxxx1001xxxxxxxxxxxxxxxxxxxx  =Load(v6)
+xxxxxxxx1001xxxxxxxxxxxxxxxxxxxx  =LoadExclusive(v6)
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  =Undefined(v6K)
-xxxxxxxx11x1xxxxxxxxxxxxxxxxxxxx  =Load(v6K)
-xxxxxxxx1011xxxxxxxxxxxxxxxxxxxx  =LoadDouble(v6K)
 xxxxxxxx1000xxxxxxxxxxxxxxxxxxxx  =StoreExclusive(v6)
 
 -- msr_and_hints (See Section A5.2.11)
@@ -110,12 +113,12 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxx
 
 -- load_store_word_byte (See Section A5.3)
 pat0(31:0)                        pat1(31:0)                        pat2(31:0)                        
+xxxxxx1xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxx1xxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxx0xxxx  =LoadRegister
 xxxxxx0xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxx0xxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  =StoreImmediate
 xxxxxx1xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxx0xxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxx0xxxx  =Forbidden
 xxxxxx0xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxx0xx1xxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  =Forbidden
 xxxxxx1xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxx0xx1xxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxx0xxxx  =Forbidden
-xxxxxx1xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxx1xxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxx0xxxx  =Load
-xxxxxx0xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxx1xxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  =Load
+xxxxxx0xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxx1xxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  =LoadImmediate
 
 -- media (See Section A5.4)
 pat0(31:0)                        pat1(31:0)                        

src/trusted/validator_arm/v2/inst_classes.h
@@ -486,30 +486,84 @@ class StoreExclusive : public ClassDecoder {
 };
 
 /*
- * The most common class of loads, where
- *  - The destination register is 15:12
- *  - The address in 19:16 is written when bit 21 is set
- *  - No flags changes or anything weird.
+ * Abstract base class for single- and double-register load instructions,
+ * below.  These instructions have common characteristics:
+ * - They aren't permitted to alter PC.
+ * - They produce a result in reg(15:12).
+ */
+class AbstractLoad : public ClassDecoder {
+ public:
+  virtual ~AbstractLoad() {}
+
+  virtual SafetyLevel safety(Instruction i) const;
+  virtual RegisterList defs(Instruction i) const;
+
+ protected:
+  bool writeback(Instruction i) const;
+};
+
+/*
+ * Loads using a register displacement, which may affect Rt (the destination)
+ * and Rn (the base address, if writeback is used).
  *
  * Notice we do not care about the width of the loaded value, because it doesn't
  * affect addressing.
  */
-class Load : public ClassDecoder {
+class LoadRegister : public AbstractLoad {
  public:
-  virtual ~Load() {}
+  virtual ~LoadRegister() {}
 
-  virtual SafetyLevel safety(Instruction i) const;
   virtual RegisterList defs(Instruction i) const;
+};
+
+/*
+ * Loads using an immediate displacement, which may affect Rt (the destination)
+ * and Rn (the base address, if writeback is used).
+ *
+ * Notice we do not care about the width of the loaded value, because it doesn't
+ * affect addressing.
+ */
+class LoadImmediate : public AbstractLoad {
+ public:
+  virtual ~LoadImmediate() {}
+
   virtual RegisterList immediate_addressing_defs(Instruction i) const;
 };
 
 /*
- * A less common class of loads, which have all the side effects of Load, but
- * also write register 15:12 + 1.
+ * Two-register immediate-offset load, which also writes Rt+1.
  */
-class LoadDouble : public Load {
+class LoadDoubleI : public LoadImmediate {
  public:
-  virtual ~LoadDouble() {}
+  virtual ~LoadDoubleI() {}
+
+  virtual RegisterList defs(Instruction i) const;
+};
+
+/*
+ * Two-register register-offset load, which also writes Rt+1.
+ */
+class LoadDoubleR : public LoadRegister {
+ public:
+  virtual ~LoadDoubleR() {}
+
+  virtual RegisterList defs(Instruction i) const;
+};
+
+/*
+ * LDREX and friends, where writeback is unavailable.
+ */
+class LoadExclusive : public AbstractLoad {
+ public:
+  virtual ~LoadExclusive() {}
+};
+
+/*
+ * LDREXD, which also writes Rt+1.
+ */
+class LoadDoubleExclusive : public LoadExclusive {
+ public:
+  virtual ~LoadDoubleExclusive() {}
 
   virtual RegisterList defs(Instruction i) const;
 };

src/trusted/validator_arm/v2/testdata/test_stores.S
@@ -45,3 +45,6 @@ bundle3:
 bundle4:
   bic r2, r2, #0          @ Mask incorrectly and
   strex r0, r1, [r2]      @ use it in a store-exclusive, for an ERROR.
+  nop
+  nop
+

commit 38c720302b35f404d9759aa38bafe58de2602227

Fixes a bug with LDRD introduced in r1562; see http://code.google.com/p/nativeclient/issues/detail?id=336 .
- Added positive/negative tests for LDRD register-indexing writeback (test_sp_updates.*).
- Noticed the problem in inst_classes.cc: in calling superclass methods from LoadDouble{I,R}, I managed to skip an entire level in the class hierarchy by calling AbstractLoad directly.  This meant the defs() implementation in Load{Immediate,Register} wasn't checked, and should have been caught by a test in r1562.

Review URL: http://codereview.chromium.org/661416

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1617 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/validator_arm/v2/inst_classes.cc
@@ -226,12 +226,12 @@ RegisterList LoadImmediate::immediate_addressing_defs(const Instruction i)
 
 
 RegisterList LoadDoubleI::defs(const Instruction i) const {
-  return AbstractLoad::defs(i) + Register(i.bits(15, 12) + 1);
+  return LoadImmediate::defs(i) + Register(i.bits(15, 12) + 1);
 }
 
 
 RegisterList LoadDoubleR::defs(const Instruction i) const {
-  return AbstractLoad::defs(i) + Register(i.bits(15, 12) + 1);
+  return LoadRegister::defs(i) + Register(i.bits(15, 12) + 1);
 }
 
 RegisterList LoadDoubleExclusive::defs(const Instruction i) const {

src/trusted/validator_arm/v2/testdata/test_sp_updates.S
@@ -62,3 +62,9 @@ bundle7:
   nop                     @ without a mask: ERROR.
   smlad sp, r0, r1, r2    @ Attempt to write SP using a MAC instruction,
   bic sp, sp, #0xC0000000 @ with a mask: ERROR - affects flags
+
+bundle8:
+  ldrd r0, [sp], r1       @ Post-index SP with a register on LDRD,
+  nop                     @ without a mask: ERROR.
+  ldrd r0, [sp], r1       @ Same case,
+  bic sp, sp, #0xC0000000 @ with a mask - should pass.

src/trusted/validator_arm/v2/testdata/test_sp_updates.err
@@ -6,3 +6,4 @@ ncval: 700000B0 6 kProblemUnsafeDataWrite 00000000
 ncval: 700000C0 6 kProblemUnsafeDataWrite 00000000
 ncval: 700000D0 6 kProblemUnsafeDataWrite 00000000
 ncval: 700000D8 6 kProblemUnsafeDataWrite 00000000
+ncval: 700000E0 6 kProblemUnsafeDataWrite 00000000