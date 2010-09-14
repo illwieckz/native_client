illwieckz/native_client

commit 40aecb1e4ed991e461c62d257db3b2f5a6e96403

Update remaining tests to build against nacl-glibc
Fix linker error with dynamic_load_test.

Fix MAXFLOAT usage in mandel_nav.c with -D_XOPEN_SOURCE.

Work around current lack of S_IFSHM in headers.  (Note that use of
S_IFSHM is itself a workaround for differences in behaviour between the
Chromium and standalone plugins.)

Don't #include <bits/mman.h>: it's a private header in glibc.

Disable code that produces warnings that become errors:
 * SEM_VALUE_MAX+1 produces an overflow warning.
 * gettimeofday(NULL, NULL) produces a NULL argument warning.

stricmp() is defined as an alias for strcasecmp() in newlib, but glibc
provides only strcasecmp().

Don't assume the 2nd argument of gettimeofday() is "void *".  In
glibc, it is "struct timezone *".

Disable two cases:  tests/toolchain and tests/nthread_nice.

TEST=python build.py -b
BUG=http://code.google.com/p/nativeclient/issues/detail?id=862

Review URL: http://codereview.chromium.org/3352021

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3279 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

tools/modular-build/build.py
@@ -374,6 +374,12 @@ int main() {
       deps=glibc_toolchain_deps + ["dummy_libnacl"],
       scons_args=["--nacl_glibc", "run_hello_world_test"])
 
+  # Check that all the Scons tests build, but don't try running them yet.
+  AddSconsModule(
+      "scons_compile_tests",
+      deps=glibc_toolchain_deps + ["nacl_libs_glibc"],
+      scons_args=["--nacl_glibc", "MODE=nacl"])
+
   return module_list
 
 

tests/dynamic_code_loading/nacl.scons
@@ -11,6 +11,10 @@ if ARGUMENTS.get('bitcode'):
 
 # This leaves a gap between the code and data segments.
 def GetLinkFlag(env):
+  if env.Bit('nacl_glibc'):
+    # When using dynamic linking, the dynamic linker is responsible
+    # for reserving this space rather than the executable.
+    return ''
   if env['TARGET_ARCHITECTURE'] == 'arm':
     # On ARM, we invoke the linker directly rather than via gcc.
     # TODO(mseaborn): Change driver script to accept -Wl args.

tests/mandel_nav/nacl.scons
@@ -44,7 +44,11 @@ mandel_nav_files = [
     'images/cycle_colors.png'
 ]
 
+# _XOPEN_SOURCE is for MAXFLOAT in glibc.  An alternative is to use
+# <values.h>, but that is not in newlib and is marked as obsolete in
+# glibc.
 env.ComponentProgram('mandel_nav.nexe', 'mandel_nav.c',
+                     CPPDEFINES=['_XOPEN_SOURCE'],
                      EXTRA_LIBS=['av', 'srpc', 'google_nacl_imc_c', 'pthread'])
 
 env.Publish('mandel_nav.nexe', 'run', mandel_nav_files)

tests/npapi_geturl/main.cc
@@ -13,6 +13,12 @@
 #include <sys/stat.h>
 #include "native_client/tests/npapi_runtime/plugin.h"
 
+#ifdef __GLIBC__
+// TODO(mseaborn): Add a NaCl-specific header, separate from
+// <sys/stat.h>, that defines this.
+# define S_IFSHM 0000240000
+#endif
+
 NPNetscapeFuncs* browser;
 
 // Create a new instance of a plugin.

tests/nthread_nice/nacl.scons
@@ -30,6 +30,11 @@
 
 Import('env')
 
+if env.Bit('nacl_glibc'):
+  # This test does not build successfully with nacl-glibc yet.
+  # TODO(mseaborn): Fix this to build.
+  Return()
+
 ######################################################################
 # Smoke Test
 ######################################################################

tests/srpc/cat.c
@@ -11,6 +11,14 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+#ifdef __GLIBC__
+/*
+ * TODO(mseaborn): Add a NaCl-specific header, separate from
+ * <sys/stat.h>, that defines this.
+ */
+# define S_IFSHM 0000240000
+#endif
+
 NaClSrpcError Cat(NaClSrpcChannel *channel,
                   NaClSrpcArg **in_args,
                   NaClSrpcArg **out_args) {

tests/sysbasic/sysbasic.c
@@ -10,8 +10,14 @@
 
 #define CHECK_ERRNO 1
 
-#include <bits/mman.h>
 #include <sys/nacl_syscalls.h>
+/*
+ * These must come after <sys/nacl_syscalls.h> when using nacl-newlib
+ * otherwise we get errors about conflicting types.  This is a bug in
+ * nacl-newlib.
+ */
+#include <sys/mman.h>
+#include <unistd.h>
 
 /* NOTE: defining CHECK_ERRNO pulls in some newlib magic (reent.h, etc.) */
 #if defined(CHECK_ERRNO)

tests/syscalls/semaphore_tests.cc
@@ -41,12 +41,15 @@ int TestSemInitErrors() {
 
   sem_t my_semaphore;
 
+  // This produces a compile-time overflow warning with glibc.
+#ifndef __GLIBC__
   // Create a value just beyond SEM_VALUE_MAX, try to initialize the semaphore.
   const unsigned int sem_max_plus_1 = SEM_VALUE_MAX + 1;
 
   // sem_init should return -1 and errno should equal EINVAL
   EXPECT(-1 == sem_init(&my_semaphore, 0, sem_max_plus_1));
   EXPECT(EINVAL == errno);
+#endif
 
   // Try with the largest possible unsigned int.
   EXPECT(-1 == sem_init(&my_semaphore,

commit 84744345095a2dabfd90ec5931661f4712538fe0

Modular build: Disable scons_compile_tests since it failed on a from-scratch build
Fixes breakage introduced in r3279.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=863
TEST="python build.py -b" after deleting out/headers*, which contained
  stale files that the build scripts do not yet correctly update
  incrementally.

Review URL: http://codereview.chromium.org/3433002

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3285 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

tools/modular-build/build.py
@@ -375,10 +375,11 @@ int main() {
       scons_args=["--nacl_glibc", "run_hello_world_test"])
 
   # Check that all the Scons tests build, but don't try running them yet.
-  AddSconsModule(
-      "scons_compile_tests",
-      deps=glibc_toolchain_deps + ["nacl_libs_glibc"],
-      scons_args=["--nacl_glibc", "MODE=nacl"])
+  # TODO(mseaborn): Enable this when it passes fully.
+  #AddSconsModule(
+  #    "scons_compile_tests",
+  #    deps=glibc_toolchain_deps + ["nacl_libs_glibc"],
+  #    scons_args=["--nacl_glibc", "MODE=nacl"])
 
   return module_list
 