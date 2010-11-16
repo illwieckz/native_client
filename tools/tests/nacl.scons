illwieckz/native_client

commit 5e6d7f90a1b21dc3917e07f7cdebcec15f36051a

Fixed bugs that prevented two unit tests from running properly.
Review URL: http://codereview.chromium.org/4832001

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3692 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

tools/tests/nacl.scons
@@ -72,13 +72,19 @@ gdb_test = env.CommandGdbTestNacl(
     command=[env.File('hello_world_gdb.nexe')],
     stdout_golden=env.File('hello_world_gdb.stdout'),
     input=env.File('hello_world_gdb.in'),
-    filter_regex=("'(Breakpoint 1 at)|"
-                   "(Breakpoint 1, )0x[0-9]+ (in hello_world)|"
-                   "^(#0 ) 0x[0-9a-f]+ (in hello_world)|"
-                   "^(#1 ) 0x[0-9a-f]+ (in main)'"
+    filter_regex=("\"^(Breakpoint 1 at)|"
+                   "^(Breakpoint 1, )0x[0-9a-fA-F]+ (in hello_world)|"
+                   "^(#0 ) 0x[0-9a-fA-F]+ (in hello_world)|"
+                   "^(#1 ) 0x[0-9a-fA-F]+ (in main)\""
                    ),
     filter_group_only='true',
     )
+
+# Currently gdb only works on Linux
+if env.Bit('host_linux'):
+  env.AddNodeToTestSuite(['hello_world_gdb.nexe', gdb_test],
+                         ['medium_tests'], 'gdb_minimal_test')
+
 
 # TODO(mseaborn): explain this magic
 if 'TRUSTED_ENV' in env:
@@ -95,17 +101,14 @@ if 'TRUSTED_ENV' in env:
         ['${SCONSTRUCT_DIR}/src/trusted/validator_x86/ncval_annotate_test.py',
          env['TRUSTED_ENV'].SubstList2('${STAGING_DIR}/ncval${PROGSUFFIX}')],
         '$PYTHON ${SOURCES[0].abspath}')]
+
+  # If we aren't running on a tester (built_elsewhere),
+  # try to use the toolchain.
+  if not ARGUMENTS.get('built_elsewhere'):
+    # Eventually we may want more SDK tests.
+    env.AddNodeToTestSuite(['libs_present_test.nexe', toolchain_tests],
+                           ['small_tests'], 'sdk_minimal_test')
+
 else:
   print ('WARNING: no trusted env specified so skipping toolchain test. '
          'Try, e.g. MODE=dbg-linux,nacl')
-  toolchain_tests = []
-
-# If we aren't running on a tester (built_elsewhere),
-# try to use the toolchain.
-if not ARGUMENTS.get('built_elsewhere'):
-  # Eventually we may want more SDK tests.
-  env.AddNodeToTestSuite(['libs_present_test.nexe', toolchain_tests],
-                         ['small_tests'], 'sdk_minimal_test')
-
-  env.AddNodeToTestSuite(['hello_world_gdb.nexe', gdb_test],
-                         [], 'gdb_minimal_test')

tests/rodata_not_writable/nacl.scons
@@ -15,4 +15,4 @@ node = env.CommandSelLdrTestNacl(
     command=[env.File('write_to_rodata.nexe')],
     exit_status='untrusted_segfault',
     stdout_golden=env.File('write_to_rodata.stdout'))
-env.AddNodeToTestSuite(node, 'small_tests', 'run_write_to_rodata_test')
+env.AddNodeToTestSuite(node, ['small_tests'], 'run_write_to_rodata_test')

commit 20668137aef4fb591499183902fab8f92ddb729a

Fixed a problem that caused sdk_minimal_test to break on a toolchain build.
This bug was introduced in r3692.

Review URL: http://codereview.chromium.org/5096001

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3698 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

tools/tests/nacl.scons
@@ -102,13 +102,21 @@ if 'TRUSTED_ENV' in env:
          env['TRUSTED_ENV'].SubstList2('${STAGING_DIR}/ncval${PROGSUFFIX}')],
         '$PYTHON ${SOURCES[0].abspath}')]
 
-  # If we aren't running on a tester (built_elsewhere),
-  # try to use the toolchain.
-  if not ARGUMENTS.get('built_elsewhere'):
-    # Eventually we may want more SDK tests.
-    env.AddNodeToTestSuite(['libs_present_test.nexe', toolchain_tests],
-                           ['small_tests'], 'sdk_minimal_test')
-
 else:
   print ('WARNING: no trusted env specified so skipping toolchain test. '
          'Try, e.g. MODE=dbg-linux,nacl')
+  toolchain_tests = []
+
+# If we aren't running on a tester (built_elsewhere),
+# try to use the toolchain.
+if not ARGUMENTS.get('built_elsewhere'):
+  # Eventually we may want more SDK tests.
+  # NOTE this test is explicitly run by the toolchain makefile in a case
+  # where toolchain_tests is empty. Do not assume the lack of dependencies
+  # means the test is disabled.
+  # NOTE this test is effectively a no-op when TRUSTED_ENV is not present and
+  # libs_present_test.nexe has already been built. If libs_present.nexe has not
+  # been built, this will force it to be built during testing.
+  env.AddNodeToTestSuite(['libs_present_test.nexe', toolchain_tests],
+                         ['small_tests'], 'sdk_minimal_test')
+