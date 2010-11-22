illwieckz/native_client

commit 01ab90c106fbb36c45a865de1c6ae9f3c7605b3a

BUG= http://code.google.com/p/nativeclient/issues/detail?id=1124
Removed code related to processing the options '-d' and '-Q' from
sel_universal, as all it did was pass them to sel_ldr in a round-about
way (and would fail in the case of '-d' because this option has been
replaced by '-a' and '-c'). Now, sel_universal and sel_ldr accept the
same command line arguments in the same order -- expect that
sel_universal now understands '--help'.

The change re-activates tests srpc_basic_test, srpc_bad_service_test,
and srpc_sysv_shm_test, which had been commented out due to the command
line incongruence between sel_ldr and sel_universal.

This change also switches sel_universal from C to C++; it now uses
sel_universal_new.cc, which can be used to compile sel_universal with
or without experimental SDL support.

Due to SCons issues and the unclear future of SDL in
sel_universal the part of the build that handles SDL is currently
inactive. See the comment in build.scons.

Review URL: http://codereview.chromium.org/5110002

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3716 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -295,11 +295,6 @@ nacl_glibc_skiplist = [
     # This uses a canned binary that is compiled w/ newlib.  A
     # glibc version might be useful.
     'run_fuzz_nullptr_test',
-    # These tests use sel_universal but that does not support the
-    # options we need (-s and -a).
-    'run_srpc_bad_service_test',
-    'run_srpc_basic_test',
-    'run_srpc_sysv_shm_test',
     # This tests the absence of "-s" but that is no good because
     # we currently force that option on.
     'run_stubout_mode_test',
@@ -958,9 +953,7 @@ def CommandSelLdrTestNacl(env, name, command,
     # TODO(mseaborn): Remove the need for the -s (stub out) option.
     sel_ldr_flags += ['-s']
 
-  # We use "-f" because sel_universal requires it, but otherwise we
-  # could use "['--'] + command" instead.
-  command = [sel_ldr] + sel_ldr_flags + ['-f', command[0], '--'] + command[1:]
+  command = [sel_ldr] + sel_ldr_flags + ['--'] + command
 
   # NOTE(robertm): log handling is a little magical
   # We do not pass these via flags because those are not usable for sel_ldr

src/trusted/sel_universal/build.scons
@@ -21,22 +21,32 @@ if env['TARGET_ARCHITECTURE'] == 'x86':
 #       on mac (nonnacl_util_c)
 
 env.ComponentProgram('sel_universal',
-                     ['sel_universal.c',
-                      ],
-                     EXTRA_LIBS=['nonnacl_util_c',
-                                 'nonnacl_srpc',
-                                 'nrd_xfer',
-                                 'nacl_base',
-                                 'platform',
-                                 'gio',
-                                 'google_nacl_imc_c',
-                                 ],
-                     FRAMEWORKS=['Cocoa'],
-                     )
+                    ['sel_universal_new.cc',
+                     ],
+                    EXTRA_LIBS=['nonnacl_util_c',
+                                'nonnacl_srpc',
+                                'nrd_xfer',
+                                'nacl_base',
+                                'platform',
+                                'gio',
+                                'google_nacl_imc_c',
+                                ],
+                    FRAMEWORKS=['Cocoa'],
+                    )
+
+######################################################################
+# TODO: factor SDL support out of sel_universal itself
+# Exiting here and thus disabling the experimental SDL build of
+# sel_universal. SCons doesn't like building several executables
+# from the same .o file (sel_universal.o) and fixing this
+# may not be worth since we want to refactor SDL support anyway.
+# (see http://www.scons.org/doc/1.1.0/HTML/scons-user/x1392.html#AEN1510)
+######################################################################
+Return()
 
 ######################################################################
 # NOTE: below is a somewhat experimental section for
-#       a multimedia enabled sel_universal_new
+#       a multimedia enabled sel_universal
 #       currently only video output is working which is enough to
 #       make earth.nexe work
 #
@@ -132,6 +142,7 @@ def MakeExtraSdlLibs(env):
 sdl_env = MakeSdlEnv(env)
 
 sdl_env.ComponentLibrary('multimedia_sdl', ['multimedia_sdl.cc'])
+sdl_env.Append(CPPDEFINES=['NACL_SEL_UNIVERSAL_INCLUDE_SDL'])
 
 # work around a cast warning in multimedia_handler.cc
 sdl_env.Append(CCFLAGS=['-fno-strict-aliasing'])
@@ -139,7 +150,7 @@ sdl_env.Append(CCFLAGS=['-fno-strict-aliasing'])
 #       anything containing SDL static
 sdl_env.FilterOut(LINKFLAGS=['-static'])
 extra_libs = MakeExtraSdlLibs(sdl_env)
-node = sdl_env.ComponentProgram('sel_universal_new',
+node = sdl_env.ComponentProgram('sel_universal_sdl',
                                 ['sel_universal_new.cc',
                                  'multimedia_handler.cc'
                                  ],

src/trusted/sel_universal/multimedia_handler.cc
@@ -140,12 +140,12 @@ static void SendVideoMemAndUpChannel(NaClSrpcChannel* channel,
 // * sets up a service for copy this video_memory into the framebuffer
 // * finally it calls the client rpc "nacl_multimedia_bridge" to communicate
 //   the shared memory area and the new service to the client
-void IntializeMultimediaHandler(NaClSrpcChannel* channel,
+void InitializeMultimediaHandler(NaClSrpcChannel* channel,
                                 int width,
                                 int height,
                                 const char* title) {
   DescWrapperFactory* factory = new DescWrapperFactory();
-  NaClLog(1, "IntializeMultimediaHandler\n");
+  NaClLog(1, "InitializeMultimediaHandler\n");
   g_mm = MakeMultimediaSDL(width, height, title);
 
   // NOTE: these are really NaClDescXferableDataDesc. Code was mimicked after
@@ -167,7 +167,7 @@ void IntializeMultimediaHandler(NaClSrpcChannel* channel,
   size_t mapped_size;
   shm->Map(reinterpret_cast<void**>(&g_video_share), &mapped_size);
 
-  NaClLog(1, "intializing the shared area at %p\n",
+  NaClLog(1, "initializing the shared area at %p\n",
           static_cast<void*>(g_video_share));
   g_video_share->u.h.revision = 0x101;
   g_video_share->u.h.video_width = width;

src/trusted/sel_universal/sel_universal.c
@@ -1,185 +1,0 @@
-/*
- * Copyright 2008 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
- */
-
-/*
- * NaCl testing shell
- */
-
-#include "native_client/src/include/nacl_macros.h"
-#include "native_client/src/include/portability.h"
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "native_client/src/shared/srpc/nacl_srpc.h"
-#include "native_client/src/trusted/nonnacl_util/sel_ldr_launcher_c.h"
-
-static const char* kUsage =
-  "Usage:\n"
-  "\n"
-  "sel_universal [option*] -f nacl_file [-- sel_ldr_arg*] [-- module_arg*]\n"
-  "\n"
-  "where option may be\n"
-  "-f nacl_module        nacl module to execute\n"
-  "-v                    increase verbosity (can be repeated)\n"
-  "\n"
-  "After startup the user is prompted for interactive commands.\n"
-  "For sample commands have a look at: tests/srpc/srpc_basic_test.stdin\n"
-  ;
-
-
-static NaClSrpcError Interpreter(NaClSrpcService* service,
-                                 NaClSrpcChannel* channel,
-                                 uint32_t rpc_number,
-                                 NaClSrpcArg** ins,
-                                 NaClSrpcArg** outs) {
-  UNREFERENCED_PARAMETER(service);
-
-  return NaClSrpcInvokeV(channel, rpc_number, ins, outs);
-}
-
-
-/* NOTE: this used to be stack allocated inside main which cause
- * problems on ARM (probably a tool chain bug).
- * NaClSrpcChannel is pretty big (> 256kB)
- */
-struct NaClSrpcChannel     command_channel;
-struct NaClSrpcChannel     channel;
-
-int main(int  argc, char *argv[]) {
-  struct NaClSelLdrLauncher* launcher;
-  int                        opt;
-  static char*               application_name = NULL;
-  int                        i;
-  size_t                     n;
-  int                        sel_ldr_argc;
-  char**                     tmp_ldr_argv;
-  const char**               sel_ldr_argv;
-  int                        module_argc;
-  const char**               module_argv;
-  static const char*         kFixedArgs[] = { "-X", "5" };
-  int                        pass_debug = 0;
-  int                        pass_pq_disable = 0;
-
-  /* Descriptor transfer requires the following. */
-  NaClNrdAllModulesInit();
-
-  /* command line parsing */
-  while ((opt = getopt(argc, argv, "df:Qv")) != -1) {
-    switch (opt) {
-      case 'd':
-        pass_debug = 1;
-        break;
-      case 'f':
-        application_name = optarg;
-        break;
-      case 'v':
-        NaClLogIncrVerbosity();
-        break;
-      case 'Q':
-        /* TODO(cbiffle): pass this in the sel_ldr flags portion of the
-           sel_universal commandline options. I.e., fix scons invocations
-           etc. so that we don't need this special case here */
-        pass_pq_disable = 1;
-        break;
-      default:
-       fputs(kUsage, stderr);
-        return -1;
-    }
-  }
-
-  if (NULL == application_name) {
-    fprintf(stderr, "-f nacl_file must be specified\n");
-    return 1;
-  }
-
-
-  /*
-   * Collect any extra arguments on to sel_ldr or the application.
-   * sel_ldr_argv contains the options to be passed to sel_ldr.
-   * module_argv contains the options to be passed to the application.
-   */
-  tmp_ldr_argv = NULL;
-  sel_ldr_argc = 0;
-  module_argv = NULL;
-  module_argc = 0;
-  for (i = 0; i < argc; ++i) {
-    if (tmp_ldr_argv == NULL) {
-      /* sel_universal arguments come first. */
-      if (!strcmp("--", argv[i])) {
-        tmp_ldr_argv = &argv[i + 1];
-      }
-    } else if (module_argv == NULL) {
-      /* sel_ldr arguments come next. */
-      if (!strcmp("--", argv[i])) {
-        module_argv = (const char**) &argv[i + 1];
-      } else {
-        ++sel_ldr_argc;
-      }
-    } else {
-      /* application arguments come last. */
-      ++module_argc;
-    }
-  }
-
-  /*
-   * Prepend the fixed arguments to the command line.
-   */
-  sel_ldr_argv =
-    (const char**) malloc((pass_debug + pass_pq_disable + sel_ldr_argc
-                           + NACL_ARRAY_SIZE(kFixedArgs)) *
-                          sizeof(*sel_ldr_argv));
-  for (n = 0; n < NACL_ARRAY_SIZE(kFixedArgs); ++n) {
-    sel_ldr_argv[n] = kFixedArgs[n];
-  }
-  for (i = 0; i < sel_ldr_argc; ++i) {
-    sel_ldr_argv[i + NACL_ARRAY_SIZE(kFixedArgs)] = tmp_ldr_argv[i];
-  }
-  if (pass_debug) {
-    sel_ldr_argv[sel_ldr_argc + NACL_ARRAY_SIZE(kFixedArgs)] = "-d";
-  }
-  if (pass_pq_disable) {
-    sel_ldr_argv[sel_ldr_argc + NACL_ARRAY_SIZE(kFixedArgs) + pass_debug] =
-        "-Q";
-  }
-
-  /*
-   * Start sel_ldr with the given application and arguments.
-   */
-  launcher = NaClSelLdrStart(application_name,
-                             5,
-                             (pass_debug + pass_pq_disable + sel_ldr_argc
-                              + NACL_ARRAY_SIZE(kFixedArgs)),
-                             (const char**) sel_ldr_argv,
-                             module_argc,
-                             (const char**) module_argv);
-
-  /*
-   * Open the communication channels to the service runtime.
-   */
-  if (!NaClSelLdrOpenSrpcChannels(launcher, &command_channel, &channel)) {
-    fprintf(stderr, "Open failed\n");
-    return 1;
-  }
-
-  NaClSrpcCommandLoop(channel.client,
-                        &channel,
-                      Interpreter,
-                      NaClSelLdrGetSockAddr(launcher));
-
-  /*
-   * Close the connections to sel_ldr.
-   */
-  NaClSrpcDtor(&command_channel);
-  NaClSrpcDtor(&channel);
-
-  /*
-   * And shut it down.
-   */
-  NaClSelLdrShutdown(launcher);
-
-  NaClNrdAllModulesFini();
-  return 0;
-}

src/trusted/sel_universal/sel_universal.gyp
@@ -37,7 +37,7 @@
       'target_name': 'sel_universal',
       'type': 'executable',
       'sources': [
-        'sel_universal.c',
+        'sel_universal_new.cc',
       ],
       'dependencies': [
         '<(DEPTH)/native_client/src/shared/imc/imc.gyp:google_nacl_imc_c',
@@ -68,7 +68,7 @@
             'win_target': 'x64',
           },
           'sources': [
-            'sel_universal.c',
+            'sel_universal_new.cc',
           ],
           'dependencies': [
             '<(DEPTH)/native_client/src/shared/imc/imc.gyp:google_nacl_imc_c64',

src/trusted/sel_universal/sel_universal_new.cc
@@ -4,14 +4,16 @@
  * be found in the LICENSE file.
  */
 
-/*
- * Second generation sel_universal implemented in C++ and with
- * (limited) multimedia support
- */
+// Second generation sel_universal implemented in C++ and with
+// (limited) multimedia support
 
-// NOTE: we need to include this so that it can "hijack" main
+#ifdef NACL_SEL_UNIVERSAL_INCLUDE_SDL
+  // NOTE: we need to include this so that it can "hijack" main
 #include <SDL.h>
+#endif
 
+#include <stdio.h>
+#include <string>
 #include <vector>
 
 #include "native_client/src/include/nacl_string.h"
@@ -22,25 +24,24 @@
 #include "native_client/src/trusted/nonnacl_util/sel_ldr_launcher.h"
 
 using std::vector;
+using std::string;
 
-
+#ifdef NACL_SEL_UNIVERSAL_INCLUDE_SDL
 // TODO(robertm): move this into its own header at some point
-extern void IntializeMultimediaHandler(NaClSrpcChannel* channel,
+extern void InitializeMultimediaHandler(NaClSrpcChannel* channel,
                                        int width,
                                        int height,
                                        const char* title);
+#endif
+
 static const char* kUsage =
   "Usage:\n"
   "\n"
-  "sel_universal_new [option*] -f nexe_file [-- sel_ldr_arg*] [-- nexe_arg*]\n"
+  "sel_universal [-f nacl_file] [sel_ldr_arg*] [-- [nacl_file] module_arg*]\n"
   "\n"
-  "where option may be\n"
-  "-f nacl_module        nacl module to execute\n"
-  "-v                    increase verbosity (can be repeated)\n"
-  "\n"
+  "Exactly one nacl_file argument is required.\n"
   "After startup the user is prompted for interactive commands.\n"
   "For sample commands have a look at: tests/srpc/srpc_basic_test.stdin\n";
-
 
 static NaClSrpcError Interpreter(NaClSrpcService* service,
                                  NaClSrpcChannel* channel,
@@ -52,99 +53,106 @@ static NaClSrpcError Interpreter(NaClSrpcService* service,
   return NaClSrpcInvokeV(channel, rpc_number, ins, outs);
 }
 
-
-/* NOTE: this used to be stack allocated inside main which cause
- * problems on ARM (probably a tool chain bug).
- * NaClSrpcChannel is pretty big (> 256kB)
- */
+// NOTE: this used to be stack allocated inside main which cause
+// problems on ARM (probably a tool chain bug).
+// NaClSrpcChannel is pretty big (> 256kB)
 struct NaClSrpcChannel command_channel;
 struct NaClSrpcChannel channel;
 
-int main(int  argc, char *argv[]) {
+// When given argc and argv this function (a) extracts the nacl_file argument,
+// (b) populates sel_ldr_argv with sel_ldr arguments, and (c) populates
+// app_argv with nexe module args. Also see kUsage above for details.
+// It will call exit with codes 0 (help message) and 1 (incorrect args).
+static nacl::string ProcessArguments(int argc,
+                                     char* argv[],
+                                     vector<nacl::string>* const sel_ldr_argv,
+                                     vector<nacl::string>* const app_argv) {
+  int i;
+
+  if (argc == 1) {
+    printf("%s", kUsage);
+    exit(0);
+  }
+
+  // Extract '-f nacl_file' from args and transfer the rest to sel_ldr_argv
   nacl::string app_name;
-  int pass_debug = 0;
-  int width = 640;
-  int height = 480;
-  /* Descriptor transfer requires the following. */
-  NaClNrdAllModulesInit();
-
-  /* command line parsing */
-  while (1) {
-    const int opt = getopt(argc, argv, "df:v");
-    if (opt == -1) break;
-
-    switch (opt) {
-      case 'd':
-        pass_debug = 1;
-        break;
-      case 'f':
-        app_name = optarg;
-        break;
-      case 'v':
-        NaClLogIncrVerbosity();
-        break;
-      default:
-        fputs(kUsage, stderr);
-        return -1;
+  for (i = 1; i < argc; i++) {
+    // Check if the argument has the form -f nacl_file
+    if (string(argv[i]) == "-f" && i + 1 < argc) {
+      app_name = argv[i + 1];
+      ++i;
+    } else if (string(argv[i]) == "--help") {
+      printf("%s", kUsage);
+      exit(0);
+    } else if (string(argv[i]) == "--") {
+      // Done processing sel_ldr args. If no '-f nacl_file' was given earlier,
+      // the first argument after '--' is the nacl_file.
+      if (app_name == "" && i + 1 < argc) {
+        app_name = argv[i + 1];
+        i++;
+      }
+      break;
+    } else {
+      sel_ldr_argv->push_back(argv[i]);
     }
+  }
+  // Postcondition: i is the index of the first nexe module argument (if any)
+  // Postcondition: sel_ldr_argv contains the arguments to sel_ldr
+
+  // Initialize the arguments to the nexe module
+  for (int j = i; j < argc; j++) {
+    app_argv->push_back(argv[j]);
   }
 
   if (app_name == "") {
-    NaClLog(LOG_FATAL, "-f nacl_file must be specified\n");
-    return 1;
+    fprintf(stderr,
+            "sel_universal: '-f nacl_file' or '-- nacl_file' required\n");
+    exit(1);
   }
 
+  return app_name;
+}
+
+
+int main(int  argc, char* argv[]) {
+  // Get the arguments to sed_ldr and the nexe module
   vector<nacl::string> sel_ldr_argv;
   vector<nacl::string> app_argv;
+  nacl::string app_name =
+    ProcessArguments(argc, argv, &sel_ldr_argv, &app_argv);
 
-  int i;
-  /* remaining sel_universal arguments come first. */
-  for (i = 0; i < argc; ++i) {
-    if (nacl::string("--") == argv[i]) break;
-  }
-
+  // Add '-X 5' to sel_ldr arguments to create communication socket
   sel_ldr_argv.push_back("-X");
   sel_ldr_argv.push_back("5");
-  if (pass_debug) {
-    sel_ldr_argv.push_back("-d");
-  }
-  /* sel_ldr arguments come next. */
-  for (++i; i < argc; ++i) {
-    if (nacl::string("--") == argv[i]) break;
 
-    sel_ldr_argv.push_back(argv[i]);
-  }
+  // Descriptor transfer requires the following
+  NaClNrdAllModulesInit();
 
-  /* app args are last. */
-  for (++i; i < argc; ++i) {
-    app_argv.push_back(argv[i]);
-  }
-
-  /*
-   * Start sel_ldr with the given application and arguments.
-   */
+  // Start sel_ldr with the given application and arguments.
   nacl::SelLdrLauncher launcher;
   if (!launcher.StartFromCommandLine(app_name, 5, sel_ldr_argv, app_argv)) {
-    NaClLog(LOG_FATAL, "Launch failed\n");
+    NaClLog(LOG_FATAL, "sel_universal: Failed to launch sel_ldr\n");
   }
 
-  /*
-   * Open the communication channels to the service runtime.
-   */
+  // Open the communication channels to the service runtime.
   if (!launcher.OpenSrpcChannels(&command_channel, &channel)) {
-    NaClLog(LOG_FATAL, "Open channel failed\n");
+    fprintf(stderr, "sel_universal: Open channel failed\n");
+    exit(1);
   }
 
-  IntializeMultimediaHandler(&channel, width, height, "NaCl Module");
+#ifdef NACL_SEL_UNIVERSAL_INCLUDE_SDL
+  int width = 640;
+  int height = 480;
+  InitializeMultimediaHandler(&channel, width, height, "NaCl Module");
+#endif
 
   NaClSrpcCommandLoop(channel.client,
                       &channel,
                       Interpreter,
                       launcher.socket_address());
 
-  /*
-   * Close the connections to sel_ldr.
-   */
+
+  // Close the connections to sel_ldr.
   NaClSrpcDtor(&command_channel);
   NaClSrpcDtor(&channel);
 

commit 347a0a1060a68e9af146e5b710f8842c1c5cb03f

Disable run_srpc_sysv_shm_test for nacl-glibc
This fixes a test failure with nacl-glibc introduced in r3716, which
enabled some tests for nacl-glibc although they did not pass yet.

This patch is prepared by Mark Seaborn.

Original ReviewURL: http://codereview.chromium.org/5240002/

Review URL: http://codereview.chromium.org/5087008

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3750 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

SConstruct
@@ -315,6 +315,11 @@ nacl_glibc_skiplist = [
     # and ncval rejects such executables.
     # See http://code.google.com/p/nativeclient/issues/detail?id=1183
     'run_whole_archive_test',
+    # run_srpc_sysv_shm_test fails because:
+    # 1) it uses fstat(), while we only have an fstat64() wrapper;
+    # 2) the test needs an explicit fflush(stdout) call because the
+    # process is killed without exit() being called.
+    'run_srpc_sysv_shm_test',
     ]
 
 