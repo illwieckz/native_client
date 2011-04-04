illwieckz/native_client

commit c83b3c47f416b5969cbcc2689fbb572c05467768

Adding additional owner to bots scripts. Fixing copyright headers on buildbot scripts. Dropping code_hygiene presubmit in favor of chrome's.
BUG=None
TEST=None
Review URL: http://codereview.chromium.org/6708128

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4674 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

buildbot/buildbot_win.bat
@@ -1,3 +1,7 @@
+rem Copyright (c) 2011 The Native Client Authors. All rights reserved.
+rem Use of this source code is governed by a BSD-style license that can be
+rem found in the LICENSE file.
+
 echo on
 
 call buildbot\msvs_env.bat
@@ -9,6 +13,11 @@ set RETCODE=0
 
 if %MODE% equ "dbg" (set GYPMODE=Debug) else (set GYPMODE=Release)
 if %BITS% equ 32 (set VCBITS=x86) else (set VCBITS=x64)
+
+rem Skip over hooks, clobber, and partial_sdk when run inside the toolchain
+rem build as the toolchain takes care or the clobber, hooks aren't needed, and
+rem partial_sdk really shouldn't be needed.
+if "%INSIDE_TOOLCHAIN%" neq "" goto SkipSync
 
 echo @@@BUILD_STEP gclient_runhooks@@@
 cmd /c gclient runhooks
@@ -26,6 +35,8 @@ call scons.bat --verbose --mode=nacl_extra_sdk platform=x86-%BITS% ^
  --download extra_sdk_update_header install_libpthread extra_sdk_update
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
+:SkipSync
+
 echo @@@BUILD_STEP gyp_compile@@@
 call vcvarsall.bat x86 && call devenv.com build\all.sln /build %GYPMODE%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
@@ -33,30 +44,6 @@ if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 echo @@@BUILD_STEP gyp_tests@@@
 python_slave.exe trusted_test.py --config %GYPMODE%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-
-echo on
-echo @@@BUILD_STEP scons_compile@@@
-call vcvarsall.bat %VCBITS% && call scons.bat -j 8 ^
- DOXYGEN=..\third_party\doxygen\win\doxygen ^
- -k --verbose --mode=%MODE%-win,nacl,doc platform=x86-%BITS%
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-
-if %BUILDBOT_BUILDERNAME% equ vista64-m64-n64-dbg goto ArchiveIt
-if %BUILDBOT_BUILDERNAME% equ vista64-m64-n64-opt goto ArchiveIt
-goto SkipArchive
-:ArchiveIt
-echo @@@BUILD_STEP archive_build@@@
-set OLD_PATH=%PATH%
-set PATH=c:\cygwin\bin;%PATH%
-tar cvfz build.tgz scons-out/
-set PATH=%OLD_PATH%
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-set HOME=c:\Users\chrome-bot
-\b\build\scripts\slave\gsutil -h Cache-Control:no-cache ^
- cp -a public-read build.tgz ^
- gs://nativeclient-archive2/between_builders/vista64_%MODE%/rev_%BUILDBOT_GOT_REVISION%/build.tgz
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-:SkipArchive
 
 echo @@@BUILD_STEP small_tests@@@
 call vcvarsall.bat %VCBITS% && call scons.bat ^
@@ -76,11 +63,38 @@ call vcvarsall.bat %VCBITS% && call scons.bat ^
  -k --verbose --mode=%MODE%-win,nacl,doc large_tests platform=x86-%BITS%
 if %ERRORLEVEL% neq 0 (set RETCODE=%ERRORLEVEL% & echo @@@BUILD_FAILED@@@)
 
+if "%INSIDE_TOOLCHAIN%" neq "" goto SkipArchive
+
 echo @@@BUILD_STEP chrome_browser_tests@@@
 call vcvarsall.bat %VCBITS% && call scons.bat ^
  DOXYGEN=..\third_party\doxygen\win\doxygen ^
  -k --verbose --mode=%MODE%-win,nacl,doc SILENT=1 platform=x86-%BITS% ^
  chrome_browser_tests
 if %ERRORLEVEL% neq 0 (set RETCODE=%ERRORLEVEL% & echo @@@BUILD_FAILED@@@)
+
+echo on
+echo @@@BUILD_STEP scons_compile@@@
+call vcvarsall.bat %VCBITS% && call scons.bat -j 8 ^
+ DOXYGEN=..\third_party\doxygen\win\doxygen ^
+ -k --verbose --mode=%MODE%-win,nacl,doc platform=x86-%BITS%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+if %BUILDBOT_BUILDERNAME% equ vista64-m64-n64-dbg goto ArchiveIt
+if %BUILDBOT_BUILDERNAME% equ vista64-m64-n64-opt goto ArchiveIt
+goto SkipArchive
+:ArchiveIt
+echo @@@BUILD_STEP archive_build@@@
+set OLD_PATH=%PATH%
+set PATH=c:\cygwin\bin;%PATH%
+tar cvfz build.tgz scons-out/
+set PATH=%OLD_PATH%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+set GS_BASE="gs://nativeclient-archive2/between_builders/vista64_%MODE%"
+set HOME=c:\Users\chrome-bot
+\b\build\scripts\slave\gsutil -h Cache-Control:no-cache ^
+ cp -a public-read build.tgz ^
+ %GS_BASE%/rev_%BUILDBOT_GOT_REVISION%/build.tgz
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+:SkipArchive
 
 exit /b %RETCODE%

buildbot/buildbot_arm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Script assumed to be run in native_client/
 if [[ $(pwd) != */native_client ]]; then

buildbot/buildbot_arm_hw.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Script assumed to be run in native_client/
 if [[ $(pwd) != */native_client ]]; then

buildbot/buildbot_lucid64-glibc-makefile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Copyright 2011 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can
-# be found in the LICENSE file.
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Script assumed to be run in native_client/
 if [[ ${PWD} != */native_client ]]; then

buildbot/buildbot_mac-glibc-makefile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Copyright 2011 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can
-# be found in the LICENSE file.
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # TODO(pasko): convert this dummy script to do something useful once it is
 # invoked on the lucid64-glibc-makefile buildbot.

buildbot/buildbot_mac.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Script assumed to be run in native_client/
 if [[ $(pwd) != */native_client ]]; then
@@ -26,6 +29,11 @@ else
   GYPMODE=Release
 fi
 
+# Skip over hooks, clobber, and partial_sdk when run inside the toolchain build
+# as the toolchain takes care or the clobber, hooks aren't needed, and
+# partial_sdk really shouldn't be needed.
+if [[ "${INSIDE_TOOLCHAIN}" != "" ]]; then
+
 echo @@@BUILD_STEP gclient_runhooks@@@
 gclient runhooks --force
 
@@ -36,6 +44,8 @@ rm -rf scons-out toolchain compiler hg ../xcodebuild ../sconsbuild ../out \
 echo @@@BUILD_STEP partial_sdk@@@
 ./scons --verbose --mode=nacl_extra_sdk platform=x86-32 --download \
 extra_sdk_update_header install_libpthread extra_sdk_update
+
+fi
 
 echo @@@BUILD_STEP gyp_compile@@@
 xcodebuild -project build/all.xcodeproj -configuration ${GYPMODE}
@@ -62,6 +72,8 @@ echo @@@BUILD_STEP large_tests@@@
     --mode=${MODE}-mac,nacl,doc large_tests platform=x86-32 || \
     (RETCODE=$? && echo @@@BUILD_FAILED@@@)
 
+if [[ "${INSIDE_TOOLCHAIN}" != "" ]]; then
+
 echo @@@BUILD_STEP begin_browser_testing@@@
 ./scons DOXYGEN=../third_party/doxygen/osx/doxygen -k --verbose \
     --mode=${MODE}-mac,nacl,doc SILENT=1 platform=x86-32 \
@@ -86,5 +98,7 @@ echo @@@BUILD_STEP end_browser_testing@@@
 ./scons DOXYGEN=../third_party/doxygen/osx/doxygen -k --verbose \
     --mode=${MODE}-mac,nacl,doc SILENT=1 platform=x86-32 \
     firefox_remove
+
+fi
 
 exit ${RETCODE}

buildbot/buildbot_pnacl1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Script assumed to be run in native_client/
 if [[ $(pwd) != */native_client ]]; then

buildbot/buildbot_pnacl2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Script assumed to be run in native_client/
 if [[ $(pwd) != */native_client ]]; then

commit 1915bea3d359c49928db5dd5be8afde8beac2e59

Do scons_compile BEFORE running tests. Broken in r4674 (by me :-( )
BUG=None
TEST=None
R=ncbray@google.com
Review URL: http://codereview.chromium.org/6720038

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4750 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

buildbot/buildbot_win.bat
@@ -45,6 +45,13 @@ echo @@@BUILD_STEP gyp_tests@@@
 python_slave.exe trusted_test.py --config %GYPMODE%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
+echo on
+echo @@@BUILD_STEP scons_compile@@@
+call vcvarsall.bat %VCBITS% && call scons.bat -j 8 ^
+ DOXYGEN=..\third_party\doxygen\win\doxygen ^
+ -k --verbose --mode=%MODE%-win,nacl,doc platform=x86-%BITS%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
 echo @@@BUILD_STEP small_tests@@@
 call vcvarsall.bat %VCBITS% && call scons.bat ^
  DOXYGEN=..\third_party\doxygen\win\doxygen ^
@@ -78,14 +85,6 @@ call vcvarsall.bat %VCBITS% && call scons.bat ^
  -k --verbose --mode=%MODE%-win,nacl,doc SILENT=1 platform=x86-%BITS% ^
  pyauto_tests
 if %ERRORLEVEL% neq 0 (set RETCODE=%ERRORLEVEL% & echo @@@STEP_FAILURE@@@)
-
-
-echo on
-echo @@@BUILD_STEP scons_compile@@@
-call vcvarsall.bat %VCBITS% && call scons.bat -j 8 ^
- DOXYGEN=..\third_party\doxygen\win\doxygen ^
- -k --verbose --mode=%MODE%-win,nacl,doc platform=x86-%BITS%
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 if %BUILDBOT_BUILDERNAME% equ vista64-m64-n64-dbg goto ArchiveIt
 if %BUILDBOT_BUILDERNAME% equ vista64-m64-n64-opt goto ArchiveIt