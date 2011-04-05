illwieckz/native_client

commit 17fe8b319e71294801e4281761d0ff665376abcb

Enable most tests for nacl64.  Six (6) tests are known to fail on linux-x86-64 for nacl-x86-64.  There is an explicit list of disabled tests in SConstruct, so we won't have to hunt through the codebase as we stabilize the system for nacl-x86-64 code.
Since the validator for nacl-x86-64 is still broken, it spews a lot of
spurious errors as well as declares that 64-bit NaCl modules unfit for
consumption.  This CL will forcibly add -d to sel_ldr when we are
running tests on nacl-x86-64 modules.  Additionally, this CL includes
changing our scons code and command_tester to accept a new
--filter_validator argument so the validator spewage can be ignored
when comparing against the golden output, and modifying sel_universal
to accept and pass on a -d flag to under underlying sel_ldr execution.

I wasted about a day trying to get scons and command_tester to play
together nicely so that python lists can be passed as command line
arguments.  I gave up after I saw how scons did not use commands.mkarg
for its escape implementation in Scons/Platform/{posix,win32}.py, but
rather used custom escaping code that's not correct -- this makes
quoting arguments properly essentially impossible (for repr(...) and
eval), since the env.subst that happens late in env.Command makes
early applications of commands.mkarg inappropriate.

Sigh.

Oh, I wasted a few hours tracking down why tests fail on hardy64 when
everything works on my machine.  The error was "Could not create
NaClElfImage object."  It seem rather weird that the machines all
start running out of memory, consistently.  It turns out it's just due
to the elf_util refactoring, error logging details got eliminated /
omitted -- what was really going on was that the trybots were
downloading the 32-bit SDK and pretending that that was the 64-bit
SDK, so whenever sel_ldr is actually asked to load a nexe, it would
complain that the ELF program header size is too small.  (Yes, there's

src/trusted/service_runtime/elf_util.c
@@ -371,7 +371,8 @@ NaClErrorCode NaClElfImageValidateProgramHeaders(
 }
 
 
-struct NaClElfImage *NaClElfImageNew(struct Gio  *gp) {
+struct NaClElfImage *NaClElfImageNew(struct Gio     *gp,
+                                     NaClErrorCode  *err_code) {
   struct NaClElfImage *result;
   struct NaClElfImage image;
   int                 cur_ph;
@@ -381,7 +382,9 @@ struct NaClElfImage *NaClElfImageNew(struct Gio  *gp) {
                         &image.ehdr,
                         sizeof image.ehdr)
       != sizeof image.ehdr) {
-    /* Consider making this fatal */
+    if (NULL != err_code) {
+      *err_code = LOAD_READ_ERROR;
+    }
     NaClLog(2, "could not load elf headers\n");
     return 0;
   }
@@ -390,12 +393,16 @@ struct NaClElfImage *NaClElfImageNew(struct Gio  *gp) {
 
   /* read program headers */
   if (image.ehdr.e_phnum > NACL_MAX_PROGRAM_HEADERS) {
-    /* Consider making this fatal */
+    if (NULL != err_code)
+      *err_code = LOAD_TOO_MANY_PROG_HDRS;
     NaClLog(2, "too many prog headers\n");
     return 0;
   }
 
   if (image.ehdr.e_phentsize < sizeof image.phdrs[0]) {
+    if (NULL != err_code) {
+      *err_code = LOAD_PROG_HDR_SIZE_TOO_SMALL;
+    }
     NaClLog(2, "bad prog headers size\n");
     NaClLog(2, " image.ehdr.e_phentsize = 0x%"PRIxElf_Half"\n",
             image.ehdr.e_phentsize);
@@ -407,6 +414,9 @@ struct NaClElfImage *NaClElfImageNew(struct Gio  *gp) {
   if ((*gp->vtbl->Seek)(gp,
                         image.ehdr.e_phoff,
                         SEEK_SET) == (size_t) -1) {
+    if (NULL != err_code) {
+      *err_code = LOAD_READ_ERROR;
+    }
     NaClLog(2, "cannot seek tp prog headers\n");
     return 0;
   }
@@ -415,6 +425,9 @@ struct NaClElfImage *NaClElfImageNew(struct Gio  *gp) {
                         &image.phdrs[0],
                         image.ehdr.e_phnum * sizeof image.phdrs[0])
       != (image.ehdr.e_phnum * sizeof image.phdrs[0])) {
+    if (NULL != err_code) {
+      *err_code = LOAD_READ_ERROR;
+    }
     NaClLog(2, "cannot load tp prog headers\n");
     return 0;
   }
@@ -429,6 +442,9 @@ struct NaClElfImage *NaClElfImageNew(struct Gio  *gp) {
   /* we delay allocating till the end to avoid cleanup code */
   result = malloc(sizeof image);
   if (result == 0) {
+    if (NULL != err_code) {
+      *err_code = LOAD_NO_MEMORY;
+    }
     NaClLog(LOG_FATAL, "no enough memory for image meta data\n");
     return 0;
   }

SConstruct
@@ -118,6 +118,20 @@ TEST_SUITES = {'all_tests': None}
 def AddNodeToTestSuite(env, node, suite_name, node_name=None):
   if not node:
     return
+
+  # TODO(bsy): these nacl64 tests are known to fail, and should be
+  # re-enabled as we get x86-64 version of sel_ldr fully operational.
+
+  known_bad_nacl64 = [ 'run_srpc_basic_test',
+                       'run_srpc_sysv_shm_test',
+                       'run_sysbasic_test',
+                       'run_thread_test',
+                       'run_simple_thread_test',
+                       'run_voronoi']
+  if node_name in known_bad_nacl64 and env['BUILD_SUBARCH'] == '64':
+    print '*** SKIPPING', node_name
+    return
+
   AlwaysBuild(node)
   env.Alias('all_tests', node)
   for s in suite_name:
@@ -444,12 +458,6 @@ def CommandSelLdrTestNacl(env, name, command,
                           loader='sel_ldr',
                           size='medium',
                           **extra):
-  # TODO(dpolukhin): remove this code when x86-64 has full sel_ldr.
-  if env['BUILD_SUBARCH'] == '64':
-    if ARGUMENTS.get('loader', 'none') != 'none':
-      loader = ARGUMENTS.get('loader')
-    else:
-      return []
 
   # NOTE: that the variable TRUSTED_ENV is set by ExportSpecialFamilyVars()
   if 'TRUSTED_ENV' not in env:
@@ -462,6 +470,13 @@ def CommandSelLdrTestNacl(env, name, command,
   trusted_env = env['TRUSTED_ENV']
   sel_ldr = trusted_env.File('${STAGING_DIR}/${PROGPREFIX}%s${PROGSUFFIX}' %
                              loader)
+
+  # TODO(bsy,kschimpf): when the validator stops producing spurious
+  # errors (bad block alignment, etc), remove the following code
+
+  if env['BUILD_SUBARCH'] == '64':
+    sel_ldr_flags = sel_ldr_flags + ['-d']
+    extra['filter_validator'] = '1'
 
   command = [sel_ldr] + sel_ldr_flags  + ['-f'] + command
 
@@ -481,7 +496,7 @@ pre_base_env.AddMethod(CommandSelLdrTestNacl)
 TEST_EXTRA_ARGS = ['stdin', 'logout',
                    'stdout_golden', 'stderr_golden', 'log_golden',
                    'stdout_filter', 'stderr_filter', 'log_filter',
-                   'osenv', 'exit_status']
+                   'osenv', 'exit_status', 'filter_validator']
 
 TEST_TIME_THRESHOLD = {
     'small':   2,
@@ -501,6 +516,7 @@ def CommandTestAgainstGoldenOutput(env, name, command, size='small',
   name = '${TARGET_ROOT}/test_results/' + name
   # NOTE: using the long version of 'name' helps distinguish opt vs dbg
   max_time = TEST_TIME_THRESHOLD[size]
+
   script_flags = ['--name', name,
                   '--report', name,
                   '--time_warning', str(max_time),

site_scons/site_tools/naclsdk.py
@@ -39,14 +39,71 @@ import sync_tgz
 
 
 NACL_PLATFORM_DIR_MAP = {
-    'win32': 'windows',
-    'cygwin': 'windows',
-    'posix': 'linux',
-    'linux': 'linux',
-    'linux2': 'linux',
-    'darwin': 'mac',
-}
+    'win32': { '32': 'windows',
+               '64': 'windows64',
+               },
+    'cygwin': { '32': 'windows',
+                '64': 'windows64',
+                },
+    'posix': { '32': 'linux',
+               '64': 'linux64',
+               },
+    'linux': { '32': 'linux',
+               '64': 'linux64',
+               },
+    'linux2': { '32': 'linux',
+                '64': 'linux64',
+                },
+    'darwin': { '32': 'mac',
+                # '64': 'mac64', # not supported!
+                },
+    }
 
+
+# TODO(bradnelson): make the snapshots be consistent, and eliminate the
+# need for this code.
+def _DefaultDownloadUrl(env):
+  """Returns the URL for downloading the SDK.
+
+  Unfortunately, the site URLs are (currently) woefully inconsistent:
+
+  http://build.chromium.org/buildbot/snapshots/nacl/
+   sdk-linux
+    naclsdk_linux.tgz
+    naclsdk_linux64.tgz
+   sdk-mac
+    naclsdk_mac.tgz
+   sdk-win
+    naclsdk_win.tgz
+   sdk-win64
+    naclsdk_win64.tgz
+
+  """
+
+  # return ('http://build.chromium.org/buildbot/snapshots/nacl/'
+  #         'naclsdk_${NATIVE_CLIENT_SDK_PLATFORM}.tgz')
+
+  canon_platform = { 'win32': 'win',
+                     'cygwin': 'win',
+                     'posix': 'linux',
+                     'linux': 'linux',
+                     'linux2': 'linux',
+                     'darwin': 'mac'}
+  plat_dir = { 'win': { '32': 'win',
+                        '64': 'win64'},
+               'linux': { '32': 'linux',
+                          '64': 'linux'},
+               'mac': {'32': 'mac'}}
+  plat_sdk = { 'win': { '32': 'win',
+                        '64': 'win64'},
+               'linux': { '32': 'linux',
+                          '64': 'linux64'},
+               'mac': {'32': 'mac'}}
+
+  p = canon_platform[env['PLATFORM']]
+  sa = env['BUILD_SUBARCH']
+  return ('http://build.chromium.org/buildbot/snapshots/nacl/sdk-'
+          + plat_dir[p][sa] + '/naclsdk_' + plat_sdk[p][sa] + '.tgz')
 
 def _GetNaclSdkRoot(env, sdk_mode):
   """Return the path to the sdk.
@@ -76,7 +133,7 @@ def _GetNaclSdkRoot(env, sdk_mode):
       return '/usr/local/nacl-sdk'
 
   elif sdk_mode == 'download':
-    platform = NACL_PLATFORM_DIR_MAP[env['PLATFORM']]
+    platform = NACL_PLATFORM_DIR_MAP[env['PLATFORM']][env['BUILD_SUBARCH']]
     root = os.path.join(env['MAIN_DIR'], 'src', 'third_party', 'nacl_sdk',
                         platform, 'sdk', 'nacl-sdk')
     return root
@@ -107,10 +164,12 @@ def DownloadSdk(env):
 
   # Get path to extract to.
   target = env.subst('$MAIN_DIR/src/third_party/nacl_sdk/' +
-                     NACL_PLATFORM_DIR_MAP[env['PLATFORM']])
+                     NACL_PLATFORM_DIR_MAP[env['PLATFORM']]
+                     [env['BUILD_SUBARCH']])
 
   # Set NATIVE_CLIENT_SDK_PLATFORM before substitution.
-  env['NATIVE_CLIENT_SDK_PLATFORM'] = NACL_PLATFORM_DIR_MAP[env['PLATFORM']]
+  env['NATIVE_CLIENT_SDK_PLATFORM'] = (NACL_PLATFORM_DIR_MAP[env['PLATFORM']]
+                                       [env['BUILD_SUBARCH']])
 
   # Allow sdk selection function to be used instead.
   if env.get('NATIVE_CLIENT_SDK_SOURCE'):
@@ -120,8 +179,7 @@ def DownloadSdk(env):
     url = [
         env.subst(env.get(
             'NATIVE_CLIENT_SDK_URL',
-            'http://build.chromium.org/buildbot/snapshots/nacl/'
-            'naclsdk_${NATIVE_CLIENT_SDK_PLATFORM}.tgz')),
+            _DefaultDownloadUrl(env))),
         env.get('NATIVE_CLIENT_SDK_USERNAME'),
         env.get('NATIVE_CLIENT_SDK_PASSWORD'),
     ]
@@ -134,17 +192,22 @@ def _SetEnvForX86Sdk(env, sdk_path):
   #       absolute path have been futile
   env.PrependENVPath('PATH', sdk_path + '/bin')
 
+  if env['BUILD_SUBARCH'] == '64':
+    naclvers = 'nacl64'
+  else:
+    naclvers = 'nacl'
+
   env.Replace(# replace hader and lib paths
-              NACL_SDK_INCLUDE=sdk_path + '/nacl/include',
-              NACL_SDK_LIB=sdk_path + '/nacl/lib',
+              NACL_SDK_INCLUDE=sdk_path + '/' + naclvers + '/include',
+              NACL_SDK_LIB=sdk_path + '/' + naclvers + '/lib',
               # Replace the normal unix tools with the NaCl ones.
-              CC='nacl-gcc',
-              CXX='nacl-g++',
-              AR='nacl-ar',
-              AS='nacl-as',
+              CC=naclvers + '-gcc',
+              CXX=naclvers + '-g++',
+              AR=naclvers + '-ar',
+              AS=naclvers + '-as',
               # NOTE: use g++ for linking so we can handle c AND c++
-              LINK='nacl-g++',
-              RANLIB='nacl-ranlib',
+              LINK=naclvers + '-g++',
+              RANLIB=naclvers + '-ranlib',
               )
 
 

src/shared/platform/linux/nacl_time.c
@@ -13,6 +13,7 @@
 #include <time.h>
 
 #include "native_client/src/include/nacl_macros.h"
+#include "native_client/src/shared/platform/nacl_log.h"
 #include "native_client/src/shared/platform/nacl_time.h"
 #include "native_client/src/shared/platform/linux/nacl_time_types.h"
 
@@ -20,6 +21,8 @@
 #include "native_client/src/trusted/service_runtime/nacl_syscall_common.h"
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
 #include "native_client/src/trusted/service_runtime/linux/nacl_syscall_inl.h"
+
+#define NANOS_PER_UNIT  (1000*1000*1000)
 
 static struct NaClTimeState gNaClTimeState;
 
@@ -75,7 +78,18 @@ int NaClNanosleep(struct nacl_abi_timespec const  *req,
   } else {
     host_remptr = &host_rem;
   }
+  NaClLog(4,
+          "nanosleep(%"PRIxPTR", %"PRIxPTR")\n",
+          (uintptr_t) &host_req,
+          (uintptr_t) host_remptr);
+  NaClLog(4, "nanosleep(time = %"PRId64".%09"PRId64" S)\n",
+          (int64_t) host_req.tv_sec, (int64_t) host_req.tv_nsec);
+  if (host_req.tv_nsec > NANOS_PER_UNIT) {
+    NaClLog(0, "tv_nsec too large %"PRId64"\n", (int64_t) host_req.tv_nsec);
+  }
   retval = nanosleep(&host_req, host_remptr);
+  NaClLog(4, " returned %d\n", retval);
+
   if (0 != retval && EINTR == errno && NULL != rem) {
     rem->tv_sec = host_rem.tv_sec;
     rem->tv_nsec = host_rem.tv_nsec;

src/trusted/sel_universal/sel_universal.c
@@ -61,14 +61,18 @@ int main(int  argc, char *argv[]) {
   int                        module_argc;
   const char**               module_argv;
   static const char*         kFixedArgs[] = { "-P", "5", "-X", "5" };
+  int                        pass_debug = 0;
 
 
   /* Descriptor transfer requires the following. */
   NaClNrdAllModulesInit();
 
   /* command line parsing */
-  while ((opt = getopt(argc, argv, "f:v")) != -1) {
+  while ((opt = getopt(argc, argv, "df:v")) != -1) {
     switch (opt) {
+      case 'd':
+        pass_debug = 1;
+        break;
       case 'f':
         application_name = optarg;
         break;
@@ -119,13 +123,17 @@ int main(int  argc, char *argv[]) {
    * Prepend the fixed arguments to the command line.
    */
   sel_ldr_argv =
-    (const char**) malloc((sel_ldr_argc + NACL_ARRAY_SIZE(kFixedArgs)) *
-                            sizeof(*sel_ldr_argv));
+    (const char**) malloc((pass_debug + sel_ldr_argc
+                           + NACL_ARRAY_SIZE(kFixedArgs)) *
+                          sizeof(*sel_ldr_argv));
   for (n = 0; n < NACL_ARRAY_SIZE(kFixedArgs); ++n) {
     sel_ldr_argv[n] = kFixedArgs[n];
   }
   for (i = 0; i < sel_ldr_argc; ++i) {
     sel_ldr_argv[i + NACL_ARRAY_SIZE(kFixedArgs)] = tmp_ldr_argv[i];
+  }
+  if (pass_debug) {
+    sel_ldr_argv[sel_ldr_argc + NACL_ARRAY_SIZE(kFixedArgs)] = "-d";
   }
 
   /*
@@ -133,7 +141,8 @@ int main(int  argc, char *argv[]) {
    */
   launcher = NaClSelLdrStart(application_name,
                              5,
-                             sel_ldr_argc + NACL_ARRAY_SIZE(kFixedArgs),
+                             (pass_debug + sel_ldr_argc
+                              + NACL_ARRAY_SIZE(kFixedArgs)),
                              (const char**) sel_ldr_argv,
                              module_argc,
                              (const char**) module_argv);

src/trusted/service_runtime/build.scons
@@ -578,7 +578,7 @@ if env['TARGET_ARCHITECTURE'] == 'x86' and env['BUILD_SUBARCH'] == '32':
 # ----------------------------------------------------------
 # Death Tests With Canned x86 Binaries
 # ----------------------------------------------------------
-ERROR_WHILE_LOADING = '"^(Error while loading).*' +  '(:[^:]*)"'
+ERROR_WHILE_LOADING = '"^(Error while loading).*' + '(:[^:]*)"'
 
 
 if env['TARGET_ARCHITECTURE'] == 'x86' and env['BUILD_SUBARCH'] == '32':

src/trusted/service_runtime/elf_util.h
@@ -17,7 +17,7 @@ uint32_t NaClElfImageGetEntryPoint(struct NaClElfImage *image);
 
 int NaClElfImageGetBundleSize(struct NaClElfImage *image);
 
-struct NaClElfImage *NaClElfImageNew(struct Gio *gp);
+struct NaClElfImage *NaClElfImageNew(struct Gio *gp, NaClErrorCode *err_code);
 
 NaClErrorCode NaClElfImageValidateElfHeader(struct NaClElfImage *image);
 

src/trusted/service_runtime/include/sys/time.h
@@ -17,7 +17,11 @@
 extern "C" {
 #endif
 
+#ifdef __native_client__
 typedef long int  nacl_abi_suseconds_t;
+#else
+typedef int32_t   nacl_abi_suseconds_t;
+#endif
 
 #ifndef nacl_abi___clock_t_defined
 #define nacl_abi___clock_t_defined
@@ -31,7 +35,11 @@ struct nacl_abi_timeval {
 
 struct nacl_abi_timespec {
   nacl_abi_time_t    tv_sec;
+#ifdef __native_client__
   long int           tv_nsec;
+#else
+  int32_t            tv_nsec;
+#endif
 };
 
 /* obsolete.  should not be used */
@@ -46,7 +54,7 @@ struct nacl_abi_timezone {
  * never be used, and defines it by default as void *.  The Mac man page says
  * it is void *.
  */
-extern int nacl_abi_gettimeofday (struct nacl_abi_timeval *tv, void *tz);
+extern int nacl_abi_gettimeofday(struct nacl_abi_timeval *tv, void *tz);
 
 #ifdef __cplusplus
 }

commit 2d1f1a443a9f54ccf2f34fe6c5ee3742834a3875

Fix bug introduced in r1480 (Seek to beginning of file to work around a Chrome issue) where NaClElfImageNew could fail (seek fails) but does not set *err_code.  This error is not actually expected to ever occur.
BUG= http://code.google.com/p/nativeclient/issues/detail?id=1604
TEST= none
Review URL: http://codereview.chromium.org/6735044

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4760 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

src/trusted/service_runtime/elf_util.c
@@ -1,7 +1,7 @@
 /*
- * Copyright 2009 The Native Client Authors.  All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 /*
@@ -398,6 +398,9 @@ struct NaClElfImage *NaClElfImageNew(struct Gio     *gp,
   memset(image.loadable, 0, sizeof image.loadable);
   if (-1 == (*gp->vtbl->Seek)(gp, 0, 0)) {
     NaClLog(2, "could not seek to beginning of Gio object containing nexe\n");
+    if (NULL != err_code) {
+      *err_code = LOAD_READ_ERROR;
+    }
     return 0;
   }
   if ((*gp->vtbl->Read)(gp,