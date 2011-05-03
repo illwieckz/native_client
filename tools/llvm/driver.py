illwieckz/native_client

commit 54e393e7845bf3173e994c3a473cfb7753085ba3

Remove nosys from the list of libs linked in by default with pnacl. Also avoid having duplicate libraries on the link line.
BUG= http://code.google.com/p/nativeclient/issues/detail?id=1755
Review URL: http://codereview.chromium.org/6903148

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5128 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

tools/llvm/driver.py
@@ -314,10 +314,10 @@ INITIAL_ENV = {
   'STDLIB_NATIVE_SUFFIX': '${ROOT_%arch%}/libcrt_platform.a ' +
                           '-L${ROOT_%arch%} -lgcc_eh -lgcc ',
 
-
   'STDLIB_BC_PREFIX': '${ROOT_BC}/nacl_startup.bc',
 
-  'STDLIB_BC_SUFFIX': '-L${ROOT_BC} -lc -lnacl ${LIBSTDCPP} -lc -lnosys',
+  'STDLIB_BC_SUFFIX':  '-L${ROOT_BC} -lc -lnacl ${LIBSTDCPP}',
+
   'LIBSTDCPP'       : '-lstdc++',
 
   # Build actual command lines below
@@ -345,11 +345,16 @@ INITIAL_ENV = {
   'LD_INPUTS' : '${STDLIB_NATIVE_PREFIX} ${inputs} ${STDLIB_NATIVE_SUFFIX}',
   'RUN_LD' : '${LD} ${LD_FLAGS} ${LD_INPUTS} -o "${output}"',
 
-  'RUN_BCLD': '${BCLD} ${BCLD_FLAGS} ' +
-              '${STDLIB_NATIVE_PREFIX} ${STDLIB_BC_PREFIX} ${inputs} ' +
-              '${STDLIB_BC_SUFFIX} ${STDLIB_NATIVE_SUFFIX} ' +
-              '${BASE}/llvm-intrinsics.bc ' +
-              '-o "${output}"',
+  'RUN_BCLD': ('${BCLD} ${BCLD_FLAGS} '
+               '${STDLIB_NATIVE_PREFIX} ${STDLIB_BC_PREFIX} '
+               '${obj_inputs} '
+               '--start-group '
+               '${lib_inputs} '
+               '${STDLIB_BC_SUFFIX} '
+               '--end-group '
+               '${STDLIB_NATIVE_SUFFIX} '
+               '${BASE}/llvm-intrinsics.bc '
+               '-o "${output}"'),
 
   'RUN_PEXECHECK': '${LLVM_LD} --nacl-abi-check ' +
                    '--nacl-legal-undefs ${PEXE_ALLOWED_UNDEFS} ${inputs}',
@@ -1191,10 +1196,20 @@ def LinkBC(inputs, output = None):
   if not arch:
     arch = 'X8632'
 
+  # separate library input from non-library inputs
+  obj_inputs = []
+  lib_inputs = []
+  for i in inputs:
+    if i.startswith('-l'):
+      lib_inputs.append(i)
+    else:
+      obj_inputs.append(i)
   # Produce combined bitcode file
-  RunWithEnv('RUN_BCLD', arch = arch,
-             inputs = shell.join(inputs),
-             output = bcld_output)
+  RunWithEnv('RUN_BCLD',
+             arch=arch,
+             obj_inputs=shell.join(obj_inputs),
+             lib_inputs=shell.join(lib_inputs),
+             output=bcld_output)
 
   #### Manually apply symbol wrap to unoptimized bitcode
   symbols = shell.split(env.get('LD_WRAP_SYMBOLS'))

commit 309a0255c82a50e89361b4652ea29cec9e78a855

Fix pnacl driver problem introduced in r5128 (gcl is broken)
git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5137 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

tools/llvm/driver.py
@@ -601,7 +601,6 @@ def PrepareFlags():
     env.clear('STDLIB_BC_SUFFIX')
     env.clear('STDLIB_NATIVE_PREFIX')
     env.clear('STDLIB_NATIVE_SUFFIX')
-    env.set('RUN_BCLD', '${RUN_LLVM_LINK}')
   else:
     # TODO(pdox): Figure out why this is needed.
     if env.getbool('GOLD_FIX'):
@@ -1200,16 +1199,22 @@ def LinkBC(inputs, output = None):
   obj_inputs = []
   lib_inputs = []
   for i in inputs:
-    if i.startswith('-l'):
+    if FileType(i) in ('bclib','nlib'):
       lib_inputs.append(i)
     else:
       obj_inputs.append(i)
+
   # Produce combined bitcode file
-  RunWithEnv('RUN_BCLD',
-             arch=arch,
-             obj_inputs=shell.join(obj_inputs),
-             lib_inputs=shell.join(lib_inputs),
-             output=bcld_output)
+  if env.getbool('SHARED'):
+    RunWithEnv('RUN_LLVM_LINK',
+               inputs=shell.join(obj_inputs),
+               output=bcld_output)
+  else:
+    RunWithEnv('RUN_BCLD',
+               arch=arch,
+               obj_inputs=shell.join(obj_inputs),
+               lib_inputs=shell.join(lib_inputs),
+               output=bcld_output)
 
   #### Manually apply symbol wrap to unoptimized bitcode
   symbols = shell.split(env.get('LD_WRAP_SYMBOLS'))