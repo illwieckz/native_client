illwieckz/native_client

commit e077af6cb9c946742a8b0881a4e40db76b732946

PNaCl: Stop system code from assuming that user code defines __nacl_read_tp
Change system code so that it defines its own __nacl_read_tp() to
resolve references generated by the PNaCl translator for the
nacl.read.tp intrinsic.

We define a __nacl_read_tp() which uses the IRT TLS interface if
available but falls back to a direct NaCl syscall.  A more ideal
approach would be to build two versions of crtbegin.c, but that would
complicate the build a lot.

Fix nacl_startup.h to #include what it uses, to get the definition of
Elf32_auxv_t.

crt1.x: Change linker script so that pexes no longer export the symbol
"__nacl_read_tp" (or "__nacl_add_tp", which appears not to be used
anyway).

build.sh:

 * Reorder so that headers like <sys/types.h> are available when we
   compile crtbegin.c.
 * Add #include path so that native_client headers are #includable in
   crtbegin.c.

BUG=https://code.google.com/p/nativeclient/issues/detail?id=3073
TEST=run_thread_test + run_thread_test_irt
  (+ remove direct-syscall fallback and check run_thread_test_irt still works)

Review URL: https://codereview.chromium.org/12706009

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10994 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

pnacl/support/crtbegin.c
@@ -20,6 +20,11 @@
 
 #include <stdint.h>
 
+#include "native_client/src/include/elf_auxv.h"
+#include "native_client/src/trusted/service_runtime/include/bits/nacl_syscalls.h"
+#include "native_client/src/untrusted/irt/irt.h"
+#include "native_client/src/untrusted/nacl/nacl_startup.h"
+
 void _pnacl_wrapper_start(uint32_t *info);
 void _start(uint32_t *info);
 
@@ -82,6 +87,40 @@ static void __attribute__((destructor)) __do_eh_dtor(void) {
 
 #else
 
+/*
+ * TODO(mseaborn): Use the definition in nacl_config.h instead.
+ * nacl_config.h is not #includable here because NACL_BUILD_ARCH
+ * etc. are not defined at this point in the PNaCl toolchain build.
+ */
+#define NACL_SYSCALL_ADDR(syscall_number) (0x10000 + (syscall_number) * 32)
+
+/*
+ * If we are not running under the IRT, we fall back to using the
+ * syscall directly.  This is to support non-IRT-based tests and the
+ * non-IRT-using sandboxed PNaCl translator.
+ */
+static void *(*g_nacl_read_tp_func)(void) =
+    (void *(*)(void)) NACL_SYSCALL_ADDR(NACL_sys_tls_get);
+
+void *__nacl_read_tp(void) {
+  return g_nacl_read_tp_func();
+}
+
+static void read_auxv(const Elf32_auxv_t *auxv) {
+  const Elf32_auxv_t *av;
+  for (av = auxv; av->a_type != AT_NULL; ++av) {
+    if (av->a_type == AT_SYSINFO) {
+      TYPE_nacl_irt_query irt_query = (TYPE_nacl_irt_query) av->a_un.a_val;
+      struct nacl_irt_tls irt_tls;
+      if (irt_query(NACL_IRT_TLS_v0_1, &irt_tls, sizeof(irt_tls))
+          == sizeof(irt_tls)) {
+        g_nacl_read_tp_func = irt_tls.tls_get;
+      }
+      return;
+    }
+  }
+}
+
 static void __do_eh_ctor(void) {
   static struct object object;
   __register_frame_info (__EH_FRAME_BEGIN__, &object);
@@ -89,6 +128,8 @@ static void __do_eh_ctor(void) {
 
 /* This defines the entry point for a nexe produced by the PNaCl translator. */
 void __pnacl_start(uint32_t *info) {
+  read_auxv(nacl_startup_auxv(info));
+
   /*
    * We must register exception handling unwind info before calling
    * any user code.  Note that we do not attempt to deregister the

pnacl/build.sh
@@ -635,8 +635,8 @@ download-toolchains() {
 #@ libs                  - install native libs and build bitcode libs
 libs() {
   libs-clean
-  libs-support newlib
   newlib portable
+  libs-support newlib
   for arch in arm x86-32 x86-64 mips32; do
     dummy-irt-shim ${arch}
   done
@@ -2981,7 +2981,7 @@ libs-support-native() {
   mkdir -p "${destdir}"
 
   local flags="--pnacl-allow-native --pnacl-allow-translate -Wall -Werror"
-  local cc_cmd="${PNACL_CC_NEUTRAL} -arch ${arch} ${flags}"
+  local cc_cmd="${PNACL_CC_NEUTRAL} -arch ${arch} ${flags} -I../../.."
 
   spushd "${PNACL_SUPPORT}"
 

pnacl/support/DEPS
@@ -1,0 +1,4 @@
+include_rules = [
+  "+native_client/src/trusted/service_runtime/include/bits/nacl_syscalls.h",
+  "+native_client/src/untrusted",
+]

pnacl/support/crt1.x
@@ -12,12 +12,5 @@
 
 EXTERN ( main exit _exit )
 
-/* Preserve __nacl_read_tp and __nacl_add_tp in the bitcode during LTO
- * (for static linking).  These functions are not referenced by the
- * program code -- references are only introduced by the compiler,
- * or by the linker (after doing TLS transitions).
- */
-EXTERN ( __nacl_read_tp __nacl_add_tp )
-
 /* These are needed by libgcc_eh. */
 EXTERN ( malloc free strlen abort )

src/untrusted/nacl/nacl_startup.h
@@ -7,6 +7,8 @@
 #ifndef NATIVE_CLIENT_SRC_UNTRUSTED_NACL_NACL_STARTUP_H_
 #define NATIVE_CLIENT_SRC_UNTRUSTED_NACL_NACL_STARTUP_H_ 1
 
+#include "native_client/src/include/elf32.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif

commit 08baabe88a17f05f6cfb850e2b1672f9f9cc8c44

Make definition of __nacl_read_tp in crtbegin.c weak
This fixes nonpexe_tests, which was broken by r10994.  (nonpexe_tests is not
run on the PNaCl toolchain buildbots or trybots.)

Calls to __nacl_read_tp may be generated by the translator. If a bitcode file
is translated to a .o and linked into a bitcode link (i.e. via
--pnacl-allow-translate), it causes the definition of __nacl_read_tp in
libnacl to get linked into the pexe, causing the native link to fail due
to duplicate definitions.

R=jvoung@chromium.org,mseaborn@chromium.org
BUG= https://code.google.com/p/nativeclient/issues/detail?id=3073

Review URL: https://codereview.chromium.org/12837010

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11003 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

pnacl/support/crtbegin.c
@@ -102,6 +102,14 @@ static void __attribute__((destructor)) __do_eh_dtor(void) {
 static void *(*g_nacl_read_tp_func)(void) =
     (void *(*)(void)) NACL_SYSCALL_ADDR(NACL_sys_tls_get);
 
+/*
+ * __nacl_read_tp is defined as a weak symbol because if a pre-translated
+ * object file (which may contain calls to __nacl_read_tp) is linked with
+ * the bitcode libnacl in nonpexe_tests, it will pull in libnacl's definition,
+ * which would then override this one at native link time rather than causing
+ * link failure.
+ */
+__attribute__((weak))
 void *__nacl_read_tp(void) {
   return g_nacl_read_tp_func();
 }