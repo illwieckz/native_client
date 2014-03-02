illwieckz/native_client

commit 67ddc4f4ef318cc00138eca12c56201695a0c1b0

Attempt to unify our platform/OS and architecture collapsing in python
Currently we have a lot of the similar code scattered across our python
code base attempting to collapse machine and/or platform names into
our own simplified names (some more complete than others). This is the
kind of thing our shared pynacl/platform.py was trying to solve. This
attempts to unify many of our use cases.

R=bradnelson@google.com, jvoung@chromium.org, dschuff@chromium.org, mseaborn@chromium.org, ncbray@chromium.org, noelallen@google.com
BUG=
TEST= trybots

Review URL: https://codereview.chromium.org/168143002

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12814 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

build/download_toolchains.py
@@ -15,8 +15,11 @@ import os
 import re
 import sys
 import tempfile
-import toolchainbinaries
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import pynacl.platform
+
+import toolchainbinaries
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(SCRIPT_DIR)
@@ -160,10 +163,9 @@ def GetUpdatedDEPS(options, versions):
     options: options from the command line.
   """
   flavors = set()
-  for platform in toolchainbinaries.PLATFORM_MAPPING:
-    pm = toolchainbinaries.PLATFORM_MAPPING[platform]
-    for arch in pm:
-      for flavor in pm[arch]:
+  for pm in toolchainbinaries.PLATFORM_MAPPING.itervalues():
+    for flavorlist in pm.itervalues():
+      for flavor in flavorlist:
         if IsFlavorNeeded(options, flavor):
           flavors.add(flavor)
   new_deps = {}
@@ -390,10 +392,11 @@ def main(args):
     print '-' * 70
     return 0
 
-  platform = download_utils.PlatformName()
-  arch = download_utils.ArchName()
+  os = pynacl.platform.GetOS()
+  arch = pynacl.platform.GetArch()
+  platform_mapping = toolchainbinaries.PLATFORM_MAPPING
   flavors = [flavor
-             for flavor in toolchainbinaries.PLATFORM_MAPPING[platform][arch]
+             for flavor in platform_mapping[os][arch]
              if IsFlavorNeeded(options, flavor)]
 
   for flavor in flavors:

SConstruct
@@ -445,14 +445,10 @@ def ExpandArguments():
 ExpandArguments()
 
 def GetTargetPlatform():
-  return ARGUMENTS.get('platform', 'x86-32')
+  return pynacl.platform.GetArch3264(ARGUMENTS.get('platform', 'x86-32'))
 
 def GetBuildPlatform():
-  arch_dict = pynacl.platform.ArchDict()
-  machine = platform.machine().lower()
-  if machine not in arch_dict:
-    raise UserError('Unrecognized host architecture: %s', platform.machine())
-  return arch_dict[machine]
+  return pynacl.platform.GetArch3264()
 
 environment_list = []
 
@@ -928,21 +924,6 @@ pre_base_env.AddMethod(Banner)
 # BUILD_ARCH: (arm, mips, x86)
 # BUILD_SUBARCH: (32, 64)
 #
-# This dictionary is used to translate from a platform name to a
-# (arch, subarch) pair
-AVAILABLE_PLATFORMS = {
-    'x86-32'      : { 'arch' : 'x86' , 'subarch' : '32' },
-    'x86-64'      : { 'arch' : 'x86' , 'subarch' : '64' },
-    'mips32'      : { 'arch' : 'mips', 'subarch' : '32' },
-    'arm'         : { 'arch' : 'arm' , 'subarch' : '32' },
-    }
-
-# Decode platform into list [ ARCHITECTURE , EXEC_MODE ].
-def DecodePlatform(platform):
-  if platform in AVAILABLE_PLATFORMS:
-    return AVAILABLE_PLATFORMS[platform]
-  raise UserError('Unrecognized platform: %s' % platform)
-
 
 DeclareBit('build_x86_32', 'Building binaries for the x86-32 architecture',
            exclusive_groups='build_arch')
@@ -973,14 +954,19 @@ def MakeArchSpecificEnv(platform=None):
   env = pre_base_env.Clone()
   if platform is None:
     platform = GetTargetPlatform()
-  info = DecodePlatform(platform)
+
+  arch = pynacl.platform.GetArch(platform)
+  if pynacl.platform.IsArch64Bit(platform):
+    subarch = '64'
+  else:
+    subarch = '32'
 
   env.Replace(BUILD_FULLARCH=platform)
-  env.Replace(BUILD_ARCHITECTURE=info['arch'])
-  env.Replace(BUILD_SUBARCH=info['subarch'])
+  env.Replace(BUILD_ARCHITECTURE=arch)
+  env.Replace(BUILD_SUBARCH=subarch)
   env.Replace(TARGET_FULLARCH=platform)
-  env.Replace(TARGET_ARCHITECTURE=info['arch'])
-  env.Replace(TARGET_SUBARCH=info['subarch'])
+  env.Replace(TARGET_ARCHITECTURE=arch)
+  env.Replace(TARGET_SUBARCH=subarch)
 
   # Example: PlatformBit('build', 'x86-32') -> build_x86_32
   def PlatformBit(prefix, platform):

build/build_nexe.py
@@ -20,6 +20,9 @@ import tempfile
 import threading
 import urllib2
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import pynacl.platform
+
 class Error(Exception):
   pass
 
@@ -32,7 +35,7 @@ def FixPath(path):
   # function that still has the path length limit. Instead, we'll cheat and
   # normalize the path lexically.
   path = os.path.normpath(os.path.join(os.getcwd(), path))
-  if sys.platform in ['win32', 'cygwin']:
+  if pynacl.platform.IsWindows():
     if len(path) > 255:
       raise Error('Path "%s" is too long (%d characters), and will fail.' % (
           path, len(path)))
@@ -130,15 +133,7 @@ class Builder(object):
     build_type = options.build.split('_')
     toolname = build_type[0]
     self.outtype = build_type[1]
-
-    if sys.platform.startswith('linux'):
-      self.osname = 'linux'
-    elif sys.platform.startswith('win'):
-      self.osname = 'win'
-    elif sys.platform.startswith('darwin'):
-      self.osname = 'mac'
-    else:
-      raise Error('Toolchain OS %s not supported.' % sys.platform)
+    self.osname = pynacl.platform.GetOS()
 
     # pnacl toolchain can be selected in three different ways
     # 1. by specifying --arch=pnacl directly to generate
@@ -503,7 +498,7 @@ class Builder(object):
     # The dependencies are whitespace delimited following the first ':'
     deps = deps.split(':', 1)[1]
     deps = deps.split()
-    if sys.platform in ['win32', 'cygwin']:
+    if pynacl.platform.IsWindows():
       deps = [self.FixWindowsPath(d) for d in deps]
     # Check if any input has changed.
     for filename in deps:

build/download_utils.py
@@ -18,31 +18,11 @@ import sys
 import time
 import urllib2
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import pynacl.platform
+
 SOURCE_STAMP = 'SOURCE_URL'
 HASH_STAMP = 'SOURCE_SHA1'
-
-
-# Designed to handle more general inputs than sys.platform because the platform
-# name may come from the command line.
-PLATFORM_COLLAPSE = {
-    'windows': 'windows',
-    'win32': 'windows',
-    'cygwin': 'windows',
-    'linux': 'linux',
-    'linux2': 'linux',
-    'linux3': 'linux',
-    'darwin': 'mac',
-    'mac': 'mac',
-}
-
-ARCH_COLLAPSE = {
-    'i386': 'x86',
-    'i686': 'x86',
-    'x86_64': 'x86',
-    'amd64': 'x86',
-    'armv7l': 'arm',
-}
-
 
 class HashError(Exception):
   def __init__(self, download_url, expected_hash, actual_hash):
@@ -53,23 +33,6 @@ class HashError(Exception):
   def __str__(self):
     return 'Got hash "%s" but expected hash "%s" for "%s"' % (
         self.actual_hash, self.expected_hash, self.download_url)
-
-
-def PlatformName(name=None):
-  if name is None:
-    name = sys.platform
-  # Default to linux, so that downloading works on *BSD.
-  return PLATFORM_COLLAPSE.get(name, 'linux')
-
-def ArchName(name=None):
-  if name is None:
-    if PlatformName() == 'windows':
-      # TODO(pdox): Figure out how to auto-detect 32-bit vs 64-bit Windows.
-      name = 'i386'
-    else:
-      import platform
-      name = platform.machine()
-  return ARCH_COLLAPSE[name]
 
 def EnsureFileCanBeWritten(filename):
   directory = os.path.dirname(filename)
@@ -164,7 +127,7 @@ def Retry(op, *args):
   # immediately, etc.
   # Virus checkers can also accidently prevent files from being deleted, but
   # that shouldn't be a problem on the bots.
-  if sys.platform in ('win32', 'cygwin'):
+  if pynacl.platform.IsWindows():
     count = 0
     while True:
       try:

build/toolchainbinaries.py
@@ -6,6 +6,11 @@
 """Utility functions and common data for downloading NaCl toolchain binaries.
 """
 
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import pynacl.platform
 
 BASE_DOWNLOAD_URL = (
     'https://storage.googleapis.com/nativeclient-archive2')
@@ -15,41 +20,41 @@ BASE_ONCE_DOWNLOAD_URL = (
 
 # TODO(dschuff): these mappings are now identical for x86 32/64. collapse them.
 PLATFORM_MAPPING = {
-    'windows': {
-        'x86': ['win_x86',
-                'win_x86_newlib',
-                'pnacl_win_x86',
-                'pnacl_translator',
-                ('win_arm_newlib',
-                 'WIN_GCC_ARM',
-                 'WIN_BINUTILS_ARM',
-                 'ALL_NEWLIB_ARM',
-                 'ALL_GCC_LIBS_ARM')],
+    pynacl.platform.OS_WIN: {
+        pynacl.platform.ARCH_X86: ['win_x86',
+                                   'win_x86_newlib',
+                                   'pnacl_win_x86',
+                                   'pnacl_translator',
+                                   ('win_arm_newlib',
+                                    'WIN_GCC_ARM',
+                                    'WIN_BINUTILS_ARM',
+                                    'ALL_NEWLIB_ARM',
+                                    'ALL_GCC_LIBS_ARM')],
     },
-    'linux': {
-        'x86': ['linux_x86',
-                'linux_x86_newlib',
-                'linux_arm_bionic',
-                'pnacl_linux_x86',
-                'linux_arm-trusted',
-                'pnacl_translator',
-                ('linux_arm_newlib',
-                 'LINUX_GCC_ARM',
-                 'LINUX_BINUTILS_ARM',
-                 'ALL_NEWLIB_ARM',
-                 'ALL_GCC_LIBS_ARM')],
-        'arm': ['pnacl_translator'],
+    pynacl.platform.OS_LINUX: {
+        pynacl.platform.ARCH_X86: ['linux_x86',
+                                   'linux_x86_newlib',
+                                   'linux_arm_bionic',
+                                   'pnacl_linux_x86',
+                                   'linux_arm-trusted',
+                                   'pnacl_translator',
+                                   ('linux_arm_newlib',
+                                    'LINUX_GCC_ARM',
+                                    'LINUX_BINUTILS_ARM',
+                                    'ALL_NEWLIB_ARM',
+                                    'ALL_GCC_LIBS_ARM')],
+        pynacl.platform.ARCH_ARM: ['pnacl_translator'],
     },
-    'mac': {
-        'x86': ['mac_x86',
-                'mac_x86_newlib',
-                'pnacl_mac_x86',
-                'pnacl_translator',
-                ('mac_arm_newlib',
-                 'MAC_GCC_ARM',
-                 'MAC_BINUTILS_ARM',
-                 'ALL_NEWLIB_ARM',
-                 'ALL_GCC_LIBS_ARM')],
+    pynacl.platform.OS_MAC: {
+        pynacl.platform.ARCH_X86: ['mac_x86',
+                                   'mac_x86_newlib',
+                                   'pnacl_mac_x86',
+                                   'pnacl_translator',
+                                   ('mac_arm_newlib',
+                                    'MAC_GCC_ARM',
+                                    'MAC_BINUTILS_ARM',
+                                    'ALL_NEWLIB_ARM',
+                                    'ALL_GCC_LIBS_ARM')],
     },
 }
 

pynacl/platform.py
@@ -7,45 +7,158 @@ import sys
 
 import lib
 
-def IsWindows():
-  return sys.platform == 'win32'
+# Define a list of supported operating systems here. The OS is based on the
+# "sys.platform" variable. The list and variable names need to be kept in sync,
+# it is written so there will be a syntax error if an item was added to 1 list
+# but not the other, although there is no check in place to be sure the order
+# is kept.
+OS_LIST = [
+    'win',
+    'mac',
+    'linux',
+]
 
-def IsMacOS():
-  return sys.platform == 'darwin'
+(
+    OS_WIN,
+    OS_MAC,
+    OS_LINUX,
+) = OS_LIST
 
-def IsLinux():
-  return sys.platform.startswith('linux')
+OS_MAP = {
+    OS_WIN: ('win', 'win32', 'cygwin'),
+    OS_MAC: ('mac', 'darwin'),
+    OS_LINUX: ('linux', 'linux2', 'linux3'),
+}
 
-# On all our supported platforms, platform.architecture()[0] returns the bitness
-# of the python binary, regardless of the OS or hardware. On OSX, a fat binary
-# that supports both will return '64bit' even if it is running in 32-bit mode.
-# On posix, platform.machine() and platform.uname()[4] return the same as
-# the system utility 'uname -m'.
-#  On linux, this is x86_64 for 64-bit distros, and i686 for 32-bit Ubuntu.
-#  Using the 'linux32' command on 64-bit ubuntu will also make it show i686.
-#  Mac OSX 10.8 returns x86_64 for all the machines we care about. On some older
-#  versions it returned i386 even on systems with 64-bit userspace support.
-# On Windows, platform.machine() and platform.uname()[4] return 'x86' if the
-# python binary is 32-bit and 'AMD64' if the python binary is 64-bit. There
-# appears to be no sane way to tell if the host OS is 64-bit (but there are
-# some insane ways). For our purposes we currently don't care.
+OS_DICT = dict([(platform, os_name)
+                for os_name, platforms in OS_MAP.iteritems()
+                for platform in platforms])
 
-# This is only used by the 32-bit linux pnacl toolchain bot, which just
-# tests that a build and run of the 32-bit toolchain works, but we don't ship
-# those binaries! TODO(dschuff): get rid of this bot or make it do something
-# useful.
-def Is64BitLinux():
-  return IsLinux() and lib.platform.machine() == 'x86_64'
+def GetOS(platform=None):
+  if platform is None:
+    platform = sys.platform
 
-def ArchDict():
-  """Returns a dictionary that maps an alias for an architecture into
-  its canonical architecture name.
-  """
-  alias = { 'x86-32': ('x86', 'x86-32', 'x86_32', 'x8632', 'i386', 'i686',
-                       'ia32', '32'),
-            'x86-64': ('x86-64', 'amd64', 'x86_64', 'x8664', '64'),
-            'arm'   : ('arm', 'armv7', 'armv7l'),
-            'mips32': ('mips32', 'mips'),
-            }
+  platform = platform.lower()
+  assert platform in OS_DICT, "Unrecognized OS platform: %s" % platform
+  return OS_DICT[platform]
 
-  return dict([(alt,arch) for arch in alias for alt in alias[arch]])
+
+def IsCygWin(platform=None):
+  if platform is None:
+    platform = sys.platform
+
+  platform = platform.lower()
+  return platform == 'cygwin'
+
+# Define a list of supported architectures here. Our architecture definition
+# comes in 2 flavors, ones that care about 32/64 bit differences, and ones that
+# do not (a flattened list). The architecture is based on the
+# "lib.platform.machine()" variable which can return many variations even on
+# the same architecture (see variations of x86-32). The list and variable names
+# needs to be kept in sync, it is written so there will be a syntax error if
+# an item was added to 1 list but not the other, although there is no check
+# in place to be sure the order is kept.
+ARCH3264_LIST = [
+    'x86-32',
+    'x86-64',
+    'arm',
+    'mips32',
+]
+
+(
+    ARCH3264_X86_32,
+    ARCH3264_X86_64,
+    ARCH3264_ARM,
+    ARCH3264_MIPS32,
+) = ARCH3264_LIST
+
+ARCH64_SET = set([ARCH3264_X86_64])
+
+ARCH3264_MAP = {
+    ARCH3264_X86_32: ('x86', 'x86-32', 'x86_32', 'x8632', 'i386',
+                      'i686', 'ia32', '32'),
+    ARCH3264_X86_64: ('x86-64', 'amd64', 'x86_64', 'x8664', '64'),
+    ARCH3264_ARM: ('arm', 'armv7', 'armv7l'),
+    ARCH3264_MIPS32: ('mips32', 'mips'),
+}
+
+ARCH3264_DICT = dict([(machine, arch_name)
+                      for arch_name, machinelist in ARCH3264_MAP.iteritems()
+                      for machine in machinelist])
+
+ARCH_LIST = [
+    'x86',
+    'arm',
+    'mips'
+]
+
+(
+    ARCH_X86,
+    ARCH_ARM,
+    ARCH_MIPS,
+) = ARCH_LIST
+
+# ARCH_MAP should be a strict flattening of ARCH3264_MAP, use that list to be
+# sure both of them contain all the variations of machine names.
+ARCH_MAP = {
+    ARCH_X86: (ARCH3264_X86_32, ARCH3264_X86_64),
+    ARCH_ARM: (ARCH3264_ARM,),
+    ARCH_MIPS: (ARCH3264_MIPS32,),
+}
+
+ARCH_DICT = dict([(arch3264_name, arch_name)
+                  for arch_name, arch3264_list in ARCH_MAP.iteritems()
+                  for arch3264_name in arch3264_list])
+
+def GetArch3264(machine=None):
+  if machine is None:
+    machine = lib.platform.machine()
+
+  machine = machine.lower()
+  assert machine in ARCH3264_DICT, "Unrecognized arch machine: %s" % machine
+  return ARCH3264_DICT[machine]
+
+def IsArch64Bit(machine=None):
+  return GetArch3264(machine) in ARCH64_SET
+
+def GetArch(machine=None):
+  arch3264 = GetArch3264(machine)
+  return ARCH_DICT[arch3264]
+
+# Here are some helper function for common checks, these should be based
+# on the generic functions above.
+def IsWindows(platform=None):
+  return GetOS(platform) == OS_WIN
+
+def IsMac(platform=None):
+  return GetOS(platform) == OS_MAC
+
+def IsLinux(platform=None):
+  return GetOS(platform) == OS_LINUX
+
+def IsLinux64(platform=None, machine=None):
+  return IsLinux(platform) and IsArch64Bit(machine)
+
+# Some of our tools utilize a unique platform string which is used to
+# distinguish between platform and architectures.
+def PlatformTriple(platform=None, machine=None):
+  os = GetOS(platform)
+  arch3264 = GetArch3264(machine)
+
+  if os == OS_WIN:
+    if IsCygWin(platform):
+      return 'i686-pc-cygwin'
+    else:
+      return 'i686-w64-mingw32'
+  elif os == OS_MAC:
+    return 'x86_64-apple-darwin'
+  elif os == OS_LINUX:
+    if arch3264 == ARCH3264_ARM:
+      # TODO(mcgrathr): How to distinguish gnueabi vs gnueabihf?
+      return 'arm-linux-gnueabihf'
+    elif arch3264 == ARCH3264_X86_32:
+      return 'i686-linux'
+    elif arch3264 == ARCH3264_X86_64:
+      return 'x86_64-linux'
+
+  raise Exception('Unknown platform and machine')

run.py
@@ -242,13 +242,14 @@ def RunSelLdr(args, quiet_args=[], collate=False, stdin_string=None):
   # The bootstrap loader args (--r_debug, --reserved_at_zero) need to
   # come before quiet_args.
   bootstrap_loader_args = []
-  if GetBuildArch().find('arm') == -1 and env.arch == 'arm':
+  arch = pynacl.platform.GetArch3264()
+  if arch != pynacl.platform.ARCH3264_ARM and env.arch == 'arm':
     prefix = [ env.qemu_arm, '-cpu', 'cortex-a9']
     if env.trace:
       prefix += ['-d', 'in_asm,op,exec,cpu']
     args = ['-Q'] + args
 
-  if GetBuildArch().find('mips32') == -1 and env.arch == 'mips32':
+  if arch != pynacl.platform.ARCH3264_MIPS32 and env.arch == 'mips32':
     prefix = [env.qemu_mips32]
     if env.trace:
       prefix += ['-d', 'in_asm,op,exec,cpu']
@@ -486,8 +487,7 @@ def ArgSplit(argv):
                       help='Run sel_ldr with debugging enabled.')
   parser.add_argument('-arch', '-m', dest='arch', action='store',
                       choices=sorted(
-                        pynacl.platform.ArchDict().keys() +
-                        ['env']),
+                        pynacl.platform.ARCH3264_LIST + ['env']),
                       help=('Specify architecture for PNaCl translation. ' +
                             '"env" is a special value which obtains the ' +
                             'architecture from the environment ' +
@@ -519,14 +519,9 @@ def ArgSplit(argv):
     # architecture, but for PNaCl we first need to translate and the
     # user didn't tell us which architecture to translate to. Be nice
     # and just translate to the current machine's architecture.
-    env.arch = GetBuildArch()
+    env.arch = pynacl.platform.GetArch()
   # Canonicalize env.arch.
-  aliases = pynacl.platform.ArchDict()
-  if env.arch in aliases:
-    env.arch = aliases[env.arch]
-  elif env.arch:
-    Fatal('Unrecognized arch "%s"!', env.arch)
-
+  env.arch = pynacl.platform.GetArch(env.arch)
   return nexe, args[1:]
 
 def Fatal(msg, *args):
@@ -603,10 +598,6 @@ def GetSconsOS():
   if 'cygwin' in name or 'windows' in name:
     return 'win'
   Fatal('Unsupported platform "%s"' % name)
-
-def GetBuildArch():
-  machine = platform.machine().lower()
-  return pynacl.platform.ArchDict()[machine]
 
 def FindBaseDir():
   '''Crawl backwards, starting from the directory containing this script,

toolchain_build/pnacl_targetlibs.py
@@ -167,15 +167,10 @@ def BuildLibgccEhCmd(sourcefile, output, arch):
     flags_naclcc = ['-arch', arch, '--pnacl-bias=' + arch,
                     '--pnacl-allow-translate', '--pnacl-allow-native']
   else:
-    if pynacl.platform.IsWindows():
-      platformdir = 'win_x86_newlib'
-    elif pynacl.platform.IsMacOS():
-      platformdir = 'mac_x86_newlib'
-    elif pynacl.platform.IsLinux():
-      platformdir = 'linux_x86_newlib'
-    else:
-      raise Exception('Unknown OS')
-    nnacl_dir = os.path.join(NACL_DIR, 'toolchain', platformdir, 'bin')
+    platform_dir = '%s_%s_newlib' % (pynacl.platform.GetOS(),
+                                     pynacl.platform.GetArch())
+
+    nnacl_dir = os.path.join(NACL_DIR, 'toolchain', platform_dir, 'bin')
     gcc_binaries = {
         'x86-32': 'i686-nacl-gcc',
         'x86-64': 'x86_64-nacl-gcc',

commit 15b3f6e0de99b72436ebc6f1575b2e16ab709322

Fix download_toolchains.py (breakage from r12814)
The script was failing with the error:

  UnboundLocalError: local variable 'os' referenced before assignment

because "os" was used as a variable shadowing "import os".

BUG=none
TEST="run build/download_toolchains.py --hash"
TBR=dyen@chromium.org

Review URL: https://codereview.chromium.org/184253006

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12818 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

build/download_toolchains.py
@@ -392,11 +392,11 @@ def main(args):
     print '-' * 70
     return 0
 
-  os = pynacl.platform.GetOS()
+  host_os = pynacl.platform.GetOS()
   arch = pynacl.platform.GetArch()
   platform_mapping = toolchainbinaries.PLATFORM_MAPPING
   flavors = [flavor
-             for flavor in platform_mapping[os][arch]
+             for flavor in platform_mapping[host_os][arch]
              if IsFlavorNeeded(options, flavor)]
 
   for flavor in flavors: